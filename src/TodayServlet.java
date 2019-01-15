a20b890c3e57f05ca5654d29079fdb569603be59
jaewon.lee <USER@AD01031807>
2019-01-15 16:53:33 +0900

    Minor Changes in CSS files

--- src/TodayServlet.java
+++ src/TodayServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class TodayServlet
  */
-@WebServlet(name = "test", urlPatterns = { "/today" })
+@WebServlet(name = "test", urlPatterns = {"/today"})
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -30,30 +30,30 @@ public class TodayServlet extends HttpServlet {
 	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+		throws ServletException, IOException {
 		// Set response content type
 		response.setContentType("text/html");
 		// Page character encoding to UTF-8 to serve Korean characters
 		response.setCharacterEncoding("UTF-8");
 		// Refreshing page every 1 second, for real-time update of the time shown
 		response.setIntHeader("Refresh", 1);
-		
+
 		// outstream of response
 		PrintWriter out = response.getWriter();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-		
+
 		//html response structure
 		String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
-		out.println(docType 
-				+ "<html>\n" + "<head><title> 현재시간 서블릿 페이지 입니다</title></head>\n"
-				+ "<h1>"
-				+ "<a href = \"/aboutme/index.html\">메인화면</a>"
-				+ "</h1>" 
-				+ "<body bgcolor = \"#f0f0f0\">\n"
-				+ "<h1 align = \"center\">"
-				+ "현재시간 : " + LocalDateTime.now().format(formatter).toString() 
-				+ "</h1>\n"
-				+ "</h2>\n" + "</body>" + "</html>");
+		out.println(docType
+			+ "<html>\n" + "<head><title> 현재시간 서블릿 페이지 입니다</title></head>\n"
+			+ "<h1>"
+			+ "<a href = \"/aboutme/index.html\">메인화면</a>"
+			+ "</h1>"
+			+ "<body bgcolor = \"#f0f0f0\">\n"
+			+ "<h1 align = \"center\">"
+			+ "현재시간 : " + LocalDateTime.now().format(formatter).toString()
+			+ "</h1>\n"
+			+ "</h2>\n" + "</body>" + "</html>");
 		out.close();
 	}
 
