bcb8cd3f9f5f29f4ff7f6e7f7183d7cbfbb460e2
JaewonLee <lee.jaewon@nts-corp.com>
2019-01-16 14:04:15 +0900

    formatting error fixed

--- src/TodayServlet.java
+++ src/TodayServlet.java
@@ -15,31 +15,31 @@ import javax.servlet.http.HttpServletResponse;
  * Author : Jaewon Lee, lee.jaewon@nts-corp.com
  */
 
-
 @WebServlet(name = "test", urlPatterns = {"/today"})
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	DateTimeFormatter timeFormatter;
-	
+
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	
+
 	// we are NOT using constructor
 	// 1. first you cannot declare constructors on interface in Java
 	// 2. Servlets are not directly instantiated by Java code, instead container create there instance and keep them in pool
-	
+
 	public TodayServlet() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	@Override
 	public void init() {
 		/**
-	     * @see  DateTimeFormatter#ofPattern();
-	     * 파라미터값에 의거하여 LocalDateTime 클래스의 값을 String으로 변환 시켜주는 포맷을 리턴한다.
-	     */
+		 * @see  DateTimeFormatter#ofPattern();
+		 * 파라미터값에 의거하여 LocalDateTime 클래스의 값을 String으로 변환 시켜주는 포맷을 리턴한다.
+		 */
 		this.timeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
 	}
 
@@ -52,7 +52,7 @@ public class TodayServlet extends HttpServlet {
 		// Response content type setting
 		response.setContentType("text/html");
 		response.setCharacterEncoding("UTF-8");
-		
+
 		// Refreshing page every 5 second, for real-time update of the time shown
 		response.setIntHeader("Refresh", 5);
 
@@ -60,32 +60,32 @@ public class TodayServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 
 		// html response structure with in-line style
-		out.println("<html>\n" + 
-			
-						"<head>"+ 
-							"<title> 현재시간 서블릿 페이지 입니다</title>"+ 
-						"</head>\n"+
-							
-						"<body bgcolor = '#f0f0f0'>\n"+
-					
-							"<h1 style= 'position: fixed; top: 10px; left: 10px; margin: 0px;'>"+
-								"<a href='/aboutme/index.html'>메인화면</a>"+
-							"</h1>"+
-								
-							"<h1 style='" + 
-								"position: relative;" + 
-								"float: left;" + 
-								"top: 50%;" + 
-								"left: 50%;" + 
-								"transform: translate(-50%, -50%);'>"+
-								"현재시간 : " + LocalDateTime.now().format(timeFormatter) + 
-							"</h1>"+
-								
-						"</body>" + 
-						
-					"</html>");
+		out.println("<html>\n" +
+
+			"<head>" +
+			"<title> 현재시간 서블릿 페이지 입니다</title>" +
+			"</head>\n" +
+
+			"<body bgcolor = '#f0f0f0'>\n" +
+
+			"<h1 style= 'position: fixed; top: 10px; left: 10px; margin: 0px;'>" +
+			"<a href='/aboutme/index.html'>메인화면</a>" +
+			"</h1>" +
+
+			"<h1 style='" +
+			"position: relative;" +
+			"float: left;" +
+			"top: 50%;" +
+			"left: 50%;" +
+			"transform: translate(-50%, -50%);'>" +
+			"현재시간 : " + LocalDateTime.now().format(timeFormatter) +
+			"</h1>" +
+
+			"</body>" +
+
+			"</html>");
 		// html output EOL
-		
+
 	}
 
 }
