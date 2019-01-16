7adf1cd963f8a7f06478172293b42aa5f176b881
임상현 <life4lord93@gmail.com>
2019-01-16 09:27:04 +0900

    주석 추가

--- src/main/java/com/nts/TodayServlet.java
+++ src/main/java/com/nts/TodayServlet.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright 2015 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
 package com.nts;
 
 import java.io.IOException;
@@ -11,6 +15,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// 현재 시간을 출력하는 Servlet
 @WebServlet("/aboutme/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -18,10 +23,17 @@ public class TodayServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
+
+		// 현재 날짜, 시간 획득
 		LocalDateTime localDateTime = LocalDateTime.now();
 
+		// 날짜, 시간을 특정 포맷으로 변환
 		String datetime = localDateTime.format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm"));
+
+		// 출력 통로 획득
 		PrintWriter out = response.getWriter();
+
+		// html 코드 출력
 		out.println("<html>");
 		out.println("<body>");
 		out.println("<a href='/index.html'>메인화면</a>");
@@ -31,6 +43,7 @@ public class TodayServlet extends HttpServlet {
 		out.println("</body>");
 		out.println("</html>");
 
+		// 통로 반환
 		out.close();
 	}
 
