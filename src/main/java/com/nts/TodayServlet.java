4cc87b194d8ae0cfb62337d1023cee9bc44c6143
박우성 <woosung.park@nts-corp.com>
2019-01-16 10:52:47 +0900

    css - min-witdh, max-width 지정, today서블릿의 css파일 추가

--- src/main/java/com/nts/TodayServlet.java
+++ src/main/java/com/nts/TodayServlet.java
@@ -13,55 +13,56 @@ import java.time.LocalDateTime;
 /**
  * Servlet implementation class TodayServlet
  */
-@WebServlet("/TodayServlet")
+@WebServlet("/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public TodayServlet() {
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * Default constructor. 
+	 */
+	public TodayServlet() {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		response.setContentType("text/html; charset=utf-8;");
-		
+
 		LocalDateTime currentTime = java.time.LocalDateTime.now();
-		
+
 		int year = currentTime.getYear();
 		int month = currentTime.getMonthValue();
 		int day = currentTime.getDayOfMonth();
 		int hour = currentTime.getHour();
 		int minute = currentTime.getMinute();
-		
-		String dateResult = "<h2>현재시간 : "+year+"/"+month+"/"+day+" "+hour+":"+minute+"</h2>";
+
+		String dateResult = "<h2>현재시간 : " + year + "/" + month + "/" + day + " " + hour + ":" + minute + "</h2>";
 
 		PrintWriter out = response.getWriter();
-		
+
 		out.println("<html>");
-		out.println("<head><meta charset=\"utf-8\">");
-		out.println("<link rel = \"stylesheet\" type = \"text/css\" href = \"date.css\">");
-		out.println("<title>Title</title></head>");
+		out.println("<head><meta charset=\"utf-8\"><title>몇시에요</title>");
+		out.println("<link href=\"css/today.css\" type=\"text/css\" rel=\"stylesheet\"");
+		out.println("</head>");
 		out.println("<body>");
 		out.println("<p><a href=\"index.html\">메인화면</a></p>");
-		out.println("<section style='text-align:center; padding:40% 0'>");
+		out.println("<section>");
 		out.println(dateResult);
 		out.println("</section>");
 		out.println("</body>");
 		out.println("</html>");
-		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
