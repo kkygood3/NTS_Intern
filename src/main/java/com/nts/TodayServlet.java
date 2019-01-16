93014890d6ec50a294e628fa0a3eebaa2c0167a3
최석현 <lkamna12@nts-corp.com>
2019-01-16 18:44:32 +0900

    불필요한 주석과 코드 제거 및 가독성 증가, Licence 삽입

--- src/main/java/com/nts/TodayServlet.java
+++ src/main/java/com/nts/TodayServlet.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright 2019 by NAVER Corp. All rights reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * @desc : 현재 시간을 알려주는 Servlet Class
+ * @file : TodayServlet.java
+ * @package : main.java.com.nts
+ * @author : Seokhyeon Choi
+ * @date : 2019. 1. 16.
+ */
+
 package main.java.com.nts;
 import java.io.PrintWriter;
 
@@ -11,40 +21,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-/**
- * Servlet implementation class TodayServlet
- */
+
 @WebServlet("/aboutme/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	private String html_send;
-	private DateTimeFormatter dtformatter;
-    public TodayServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-  
+	private String htmlSendFrame;
+	private DateTimeFormatter dtFormatter;
+	
     public void init(ServletConfig config)throws ServletException{
-    	html_send=new String();
-    	html_send+="<a href=\"/aboutme/index.html\"><h2>메인화면</h2></a>\n";
-		html_send+="<h1 style=\"text-align:center; margin-top:150px;\">";
-		html_send+="현재시간 : ";
+    	htmlSendFrame = new String();
+    	htmlSendFrame += "<a href=\"/aboutme/index.html\"><h2>메인화면</h2></a>\n";
+    	htmlSendFrame += "<h1 style=\"text-align:center; margin-top:150px;\">";
+    	htmlSendFrame += "현재시간 : ";
 		
-		dtformatter = DateTimeFormatter.ofPattern("y/M/d H:m");
+    	dtFormatter = DateTimeFormatter.ofPattern("y/M/d H:m");
     }
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
 		response.setContentType("text/html;charset=utf-8;");
+		
+		String htmlSend = new String(htmlSendFrame);
+		htmlSend += LocalDateTime.now().format(dtFormatter);
+		htmlSend += "</h1>\n";
+		
 		PrintWriter out = response.getWriter();
-		
-		String s = new String(html_send);
-		s+=LocalDateTime.now().format(dtformatter);
-		s+="</h1>\n";
-		
-		out.print(s);
+		out.print(htmlSend);
 		out.close();
 	}
-
 }
