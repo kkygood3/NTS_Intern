048f48373002ba77df8d743c3e04ec062450fb8f
박두익 <duik.park@nts-corp.com>
2019-01-16 18:27:26 +0900

    TodayServlet.java 현재시간 출력완료

--- src/main/java/com/nts/TodayServlet.java
+++ src/main/java/com/nts/TodayServlet.java
@@ -1,23 +1,28 @@
 package main.java.com.nts;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Servlet implementation class TodayServlet
  */
-@WebServlet("/TodayServlet")
+@WebServlet("/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TodayServlet() {
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public TodayServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,6 +32,13 @@ public class TodayServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		LocalDateTime dateTime = LocalDateTime.now();
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/M/d hh:mm");
+		String dateTimeString = dateTime.format(dateTimeFormatter);
+		out.print("현재시간 : "+dateTimeString);
+		out.close();
 	}
 
 }
