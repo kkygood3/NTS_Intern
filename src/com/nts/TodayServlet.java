614218e0696da536101ba09e6481252cefe56035
siyoon <c.yun@nts-corp.com>
2019-01-17 17:43:02 +0900

    주석추가, 불필요한 필드 제거, UI수정

--- src/com/nts/TodayServlet.java
+++ src/com/nts/TodayServlet.java
@@ -2,9 +2,7 @@
  * Copyright 2019 Naver Corp. All rights Reserved.
  * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
-
 package com.nts;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
@@ -16,11 +14,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/*
+ * 현재 시간을 표시하는 html페이지를 생성하는 클래스
+ * @author si yoon
+ */
 @WebServlet("/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	/*
-	 * 현재 시간을 표시
+	 * LocalDateTime으로 현재 시간을 표시
 	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
 	 * 
 	 */
@@ -30,20 +32,16 @@ public class TodayServlet extends HttpServlet {
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 
-		out.print("<meta charset='utf-8'>");
-		out.print("<link rel='stylesheet' href='./css/today.css'>");
-
 		out.print("<!doctype html>");
 		out.print("<html>");
 		out.print("<head>");
+		out.print("<meta charset='utf-8'>");
+		out.print("<link rel='stylesheet' href='./css/today.css'>");
 		out.print("<title>now time</title>");
 		out.print("</head>");
-
 		out.print("<body>");
 		out.print("<a href='./index.html'>메인으로</a>");
-
 		out.print("<h1><center>현재시간 : " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-M-d HH:mm")) + "<center><h1>");
-
 		out.print("</body>");
 		out.print("</html>");
 
