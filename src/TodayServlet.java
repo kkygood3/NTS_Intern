5e3fe3396d6fd231bf7ab9ea3dd1034807aca98c
yongjoon.park <USER@AD01031808>
2019-01-17 13:25:02 +0900

    Servlet 수정 모호한 변수명, 필요없는 코드 삭제, 재사용 고려한 전역변수 활용

--- src/TodayServlet.java
+++ src/TodayServlet.java
@@ -4,37 +4,32 @@ import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "today", urlPatterns = {"/today"})
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public TodayServlet() {
-		super();
-	}
+	DateTimeFormatter dateTimeformatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-		
+	public TodayServlet() {}
+
+	protected void doGet(HttpServletResponse response)
+		throws IOException {
+
 		// 기본 설정
 		response.setContentType("text/html");
 		response.setCharacterEncoding("UTF-8");
-		
-		
+
 		PrintWriter writer = response.getWriter();
 		writer.print("<a href='view/index.html'>메인화면</a>");
 		writer.print("<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>");
-		
-		// 시간 설정 및 출력
-		LocalDateTime time = LocalDateTime.now();
-		DateTimeFormatter dateTimeformat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
-		String currentTime = "현재시간 : " + time.format(dateTimeformat);
-		writer.print("<h1 style='text-align: center;'>" + currentTime + "</h1>");
 
+		// 시간 설정 및 출력
+		LocalDateTime localDateTime = LocalDateTime.now();
+		String currentTime = "현재시간 : " + localDateTime.format(dateTimeformatter);
+		writer.print("<h1 style='text-align: center;'>" + currentTime + "</h1>");
 	}
 }
