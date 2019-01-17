ef052c661f173071e1b84fd9944777cd695807c3
박우성 <woosung.park@nts-corp.com>
2019-01-17 13:54:07 +0900

    프로젝트, java 라이센스 추가, javadoc 주석과 doc폴더 생성

---
+++ LICENSE.md
@@ -0,0 +1,5 @@
+Copyright 2019 NAVER Corp.
+All rights reserved.
+
+Except in the case of internal use for NAVER,
+unauthorized use or redistribution of this software are strongly prohibited.
--- src/main/java/com/nts/TodayServlet.java
+++ src/main/java/com/nts/TodayServlet.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright 2019 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
 package com.nts;
 
 import java.io.IOException;
@@ -11,20 +15,26 @@ import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+
+/**
+ * '지금몇시' 링크를 선택할 때 보여주는 현재시간 페이지의 서블렛 클래스 
+ * @author  박우성
+ */
 @WebServlet("/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	public TodayServlet() {
-	}
-
+	
+    /** 
+     * 현재시간 페이지를 출력
+     */  
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
 		response.setContentType("text/html; charset=utf-8;");
 
 		LocalDateTime currentTime = java.time.LocalDateTime.now();
 
-		String dateResult = "<h2 class=\"date\">현재시간 : " + currentTime.format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm"))+ "</h2>";
+		String dateResult = "<h2 class=\"date\">현재시간 : "
+			+ currentTime.format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm")) + "</h2>";
 
 		PrintWriter out = response.getWriter();
 
@@ -39,10 +49,10 @@ public class TodayServlet extends HttpServlet {
 		out.println("</section>");
 		out.println("</body>");
 		out.println("</html>");
-		
+
 		out.close();
 	}
-
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
 		doGet(request, response);
