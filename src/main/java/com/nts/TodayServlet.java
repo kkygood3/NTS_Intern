e25446cfae48155daa74ca0c82c8d3f537869bf7
임상현 <life4lord93@gmail.com>
2019-01-17 18:56:14 +0900

    주석 추가, 출력 ui 변경(시간이 화면 정가운데 표시되도록)

--- src/main/java/com/nts/TodayServlet.java
+++ src/main/java/com/nts/TodayServlet.java
@@ -16,7 +16,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- *  /aboutme/today url로 접근시 현재 시간이 출력되는 페이지를 응답하는 Servlet
+ *  @author 임상현, life4lord93@nts-corp.com
+ *  @description /aboutme/today url로 접근시 현재 시간이 출력되는 페이지를 응답하는 Servlet
  *   
  *	dateTimeFormat을 다양하게 써야할 경우 enum으로 분리하여 사용하는 방법이 좋아보임.
  */
@@ -37,16 +38,19 @@ public class TodayServlet extends HttpServlet {
 		response.setContentType("text/html;charset=UTF-8");
 
 		LocalDateTime nowDateTime = LocalDateTime.now();
-		
+
 		// yyyy/MM/dd HH:mm 형식
 		String datetime = nowDateTime.format(yyyyMMddHHmmFormatter);
 
 		PrintWriter out = response.getWriter();
 
-		out.println("<html>");
+		out.println("<html lang='ko'>");
+		out.println("<head>");
+		out.println("<link rel='stylesheet' type='text/css' href='/css/common.css'/>");
+		out.println("</head>");
 		out.println("<body>");
 		out.println("<a href='/index.html'>메인화면</a>");
-		out.print("<h1>");
+		out.print("<h1 class='center'>");
 		out.print("현재시간 : ");
 		out.print(datetime);
 		out.print("</h1>");
