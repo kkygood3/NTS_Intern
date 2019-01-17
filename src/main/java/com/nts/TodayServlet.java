e9848f2fc7d6d348e18ee6e1a4998c2edb268557
이승수 <lee-seungsoo@nts-corp.com>
2019-01-17 12:26:09 +0900

    코딩컨벤션 적용

--- src/main/java/com/nts/TodayServlet.java
+++ src/main/java/com/nts/TodayServlet.java
@@ -41,10 +41,10 @@ public class TodayServlet extends HttpServlet {
 		String formattedTime = currentTime.format(timeFormat);
 
 		PrintWriter out = response.getWriter();
-		out.println("<link rel='stylesheet' type='text/css' href='css/layout.css'>"
-			+ "<body class='servlet_body'>"
-			+ "<a href='index.html'>메인화면</a>"
-			+ "<h1 class='current_time'> 현재시간 : " + formattedTime + "</h1>"
+		out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"css/layout.css\">"
+			+ "<body class=\"servlet_body\">"
+			+ "<a href=\"index.html\">메인화면</a>"
+			+ "<h1 class=\"current_time\"> 현재시간 : " + formattedTime + "</h1>"
 			+ "</body>");
 	}
 }
