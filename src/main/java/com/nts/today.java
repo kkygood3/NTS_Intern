ef26eba0907a9a15c7fcbb9f312c204d944b3794
siyoon <c.yun@nts-corp.com>
2019-01-17 12:13:39 +0900

    불필요한 import, comment 제거

--- src/main/java/com/nts/today.java
+++ src/main/java/com/nts/today.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import javafx.util.converter.LocalDateTimeStringConverter;
-
 import java.time.LocalDateTime;
 
 @WebServlet("/today")
@@ -19,7 +17,6 @@ public class today extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 
 		LocalDateTime now = LocalDateTime.now();
 		response.setContentType("text/html;charset=UTF-8");
