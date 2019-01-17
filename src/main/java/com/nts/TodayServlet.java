d6bb88d29b066cbb5aaa36bfbd9fbc80952535a8
최석현 <lkamna12@nts-corp.com>
2019-01-17 11:38:32 +0900

    Class 및 Method 주석 삽입

--- src/main/java/com/nts/TodayServlet.java
+++ src/main/java/com/nts/TodayServlet.java
@@ -1,11 +1,6 @@
-/**
+/*
  * Copyright 2019 by NAVER Corp. All rights reserved.
  * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- * @desc : 현재 시간을 알려주는 Servlet Class
- * @file : TodayServlet.java
- * @package : main.java.com.nts
- * @author : Seokhyeon Choi
- * @date : 2019. 1. 16.
  */
 
 package main.java.com.nts;
@@ -22,13 +17,30 @@ import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+
+/**
+ *
+ * @description : 현재 시간을 알려주는 Servlet Class
+ * @filename : TodayServlet.java
+ * @package : main.java.com.nts
+ * 
+ * @author : Seokhyeon Choi
+ * @date : 2019. 1. 17.
+ */
 @WebServlet("/aboutme/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	  
 	private String htmlSendFrame;
 	private DateTimeFormatter dtFormatter;
-	
+    
+    /**
+     * @description : 출력할 HTML Frame 생성 및 구조 정의
+     * @method Name : init
+     * @input : ServletConfig
+     * @return : void
+     * @see javax.servlet.GenericServlet#init(javax.servlet.ServletConfig)
+     */
     public void init(ServletConfig config)throws ServletException{
     	htmlSendFrame = new String();
     	htmlSendFrame += "<a href=\"/2019_1st_intern/aboutme/index.html\"><h2>메인화면</h2></a>\n";
@@ -38,6 +50,13 @@ public class TodayServlet extends HttpServlet {
     	dtFormatter = DateTimeFormatter.ofPattern("y/M/d H:m");
     }
      
+	/**
+	 * @description : HttpServletResponse에 현재 시간을 HTML로 출력
+	 * @method Name : doGet
+	 * @input : HttpServletRequest, HttpServletResponse
+	 * @return : void
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8;");
 		
