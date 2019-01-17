e6dd5a34915dcd5921ab7ab5a3441e248c8d89aa
siyoon <c.yun@nts-corp.com>
2019-01-17 17:23:30 +0900

    UI 수정

--- src/com/nts/TodayServlet.java
+++ src/com/nts/TodayServlet.java
@@ -31,7 +31,6 @@ public class TodayServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 
 		out.print("<meta charset='utf-8'>");
-		out.print("<link rel='stylesheet' href='./css/layout.css'>");
 		out.print("<link rel='stylesheet' href='./css/today.css'>");
 
 		out.print("<!doctype html>");
@@ -41,21 +40,10 @@ public class TodayServlet extends HttpServlet {
 		out.print("</head>");
 
 		out.print("<body>");
-		out.print("	<header>");
-		out.print("		<ul>");
-		out.print("			<li><a href='./index.html'>홈</a></li>");
-		out.print("			<li><a href='./aboutme.html'>자기소개</a></li>");
-		out.print("			<li><a href='./photo.html'>내사진</a></li>");
-		out.print("			<li><a href='./today'>몇시에요</a></ul>");
-		out.print("		</ul>");
-		out.print("	</header>");
-		out.print("	<div class='content'>");
-		out.print(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-M-d HH:mm")));
-		out.print("	</div>");
-		out.print("	<footer>");
-		out.print("		<p>crong@codesdj.fff</p>");
-		out.print("		<p>http://aaa.aaa.aaa</p>");
-		out.print("	</footer>");
+		out.print("<a href='./index.html'>메인으로</a>");
+
+		out.print("<h1><center>현재시간 : " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-M-d HH:mm")) + "<center><h1>");
+
 		out.print("</body>");
 		out.print("</html>");
 
