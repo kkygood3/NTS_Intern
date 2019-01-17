cf2b5ec3c489600c2b83fbec68a1bfe7685d1d05
최석현 <lkamna12@nts-corp.com>
2019-01-17 10:57:32 +0900

    HTML/CSS 가독성 증가, Style CSS 통합 관리

--- src/main/java/com/nts/TodayServlet.java
+++ src/main/java/com/nts/TodayServlet.java
@@ -25,19 +25,19 @@ import java.time.format.DateTimeFormatter;
 @WebServlet("/aboutme/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+	  
 	private String htmlSendFrame;
 	private DateTimeFormatter dtFormatter;
 	
     public void init(ServletConfig config)throws ServletException{
     	htmlSendFrame = new String();
-    	htmlSendFrame += "<a href=\"/aboutme/index.html\"><h2>메인화면</h2></a>\n";
+    	htmlSendFrame += "<a href=\"/2019_1st_intern/aboutme/index.html\"><h2>메인화면</h2></a>\n";
     	htmlSendFrame += "<h1 style=\"text-align:center; margin-top:150px;\">";
     	htmlSendFrame += "현재시간 : ";
 		
     	dtFormatter = DateTimeFormatter.ofPattern("y/M/d H:m");
     }
-    
+     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8;");
 		
