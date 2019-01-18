a59fd23c21429ad2ec902b9b35ece0dd273a794f
Yook Sung Ryul <yook0328@gmail.com>
2019-01-18 14:53:54 +0900

    Todo 모델 클래스 제작.

---
+++ src/main/java/com/nts/jdbc/dto/Todo.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2019 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.nts.jdbc.dto;
+
+import java.util.Date;
+
+public class Todo {
+	private int id;
+	private String title;
+	private String name;
+	private int sequence;
+	private String type;
+	private Date regDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSequence() {
+		return sequence;
+	}
+
+	public void setSequence(int sequence) {
+		this.sequence = sequence;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+}
