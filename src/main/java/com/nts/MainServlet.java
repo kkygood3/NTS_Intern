68e221121c1e23e05a5b4af7ca37235b88070d40
Yook Sung Ryul <yook0328@gmail.com>
2019-01-21 15:43:28 +0900

    불러온 리스트 분할 방식 변경.

--- src/main/java/com/nts/MainServlet.java
+++ src/main/java/com/nts/MainServlet.java
@@ -7,7 +7,6 @@ package com.nts;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -29,40 +28,16 @@ public class MainServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
 
-		boolean success = false;
-		List<Todo> todoList = new ArrayList<>();
-		List<Todo> doingList = new ArrayList<>();
-		List<Todo> doneList = new ArrayList<>();
 		TodoDao todoDao = TodoDaoProvider.getTodoDaoInstance();
 
 		try {
 			List<Todo> list = todoDao.getTodos();
-			divideTodoList(list, todoList, doingList, doneList);
-			success = true;
+			request.setAttribute("TodoList", list);
 		} catch (SQLException e) {
 			e.printStackTrace();
+			response.sendError(response.SC_BAD_REQUEST, e.getMessage());
 		}
-
-		request.setAttribute("Success", success);
-		request.setAttribute("TodoList", todoList);
-		request.setAttribute("DoingList", doingList);
-		request.setAttribute("DoneList", doneList);
-
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/main.jsp");
 		requestDispatcher.forward(request, response);
 	}
-
-	void divideTodoList(List<Todo> originList, List<Todo> todoList, List<Todo> doingList, List<Todo> doneList) {
-
-		for (Todo todo : originList) {
-			if (todo.getType().equals(TodoTag.TODO.getTodoTag())) {
-				todoList.add(todo);
-			} else if (todo.getType().equals(TodoTag.DOING.getTodoTag())) {
-				doingList.add(todo);
-			} else {
-				doneList.add(todo);
-			}
-		}
-	}
-
 }
