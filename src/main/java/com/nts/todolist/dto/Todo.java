04cde5ed041ef0a74666701e3ff8e3c5c0f3cd18
yongjoon.park <USER@AD01031808>
2019-01-21 18:47:36 +0900

    Todo DTO 생성

---
+++ src/main/java/com/nts/todolist/dto/Todo.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright 2019 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.nts.todolist.dto;
+
+/**
+ * @author yongjoon.Park
+*/
+public class Todo {
+	private long id;
+	private String title;
+	private String name;
+	private int sequence;
+	private String type;
+	private String regdate;
+
+	public Todo() {}
+
+	public Todo(long id, String title, String name, int sequence, String type, String regdate) {
+		this.id = id;
+		this.title = title;
+		this.name = name;
+		this.sequence = sequence;
+		this.type = type;
+		this.regdate = regdate;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSequence() {
+		return sequence;
+	}
+
+	public void setSequence(int sequence) {
+		this.sequence = sequence;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+
+	@Override
+	public String toString() {
+		return "Todo [id=" + id + ", title=" + title + ", name=" + name + ", sequence=" + sequence + ", type=" + type
+			+ ", regdate=" + regdate + "]";
+	}
+
+}
