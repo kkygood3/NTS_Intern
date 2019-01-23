2583c79b7e5bbad2a3729e6b836ded1fd486ab21
임상현 <life4lord93@gmail.com>
2019-01-23 17:38:04 +0900

    주석, 상수 소문자->대문자 변환

--- src/main/java/com/nts/factory/MysqlConnectionFactory.java
+++ src/main/java/com/nts/factory/MysqlConnectionFactory.java
@@ -8,13 +8,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * @author 임상현, life4lord93@nts-corp.com
+ * connection을 만드는 정보를 가지고, 커넥션을 생성해줌
+ */
 public class MysqlConnectionFactory {
-	private static final String url = "jdbc:mysql://10.113.116.52:13306/user10";
-	private static final String user = "user10";
-	private static final String password = "user!@10";
+	private static final String URL = "jdbc:mysql://10.113.116.52:13306/user10";
+	private static final String USER = "user10";
+	private static final String PASSWORD = "user!@10";
 
 	public static Connection getConnection() throws SQLException {
-		Connection connection = DriverManager.getConnection(url, user, password);
+		Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
 		return connection;
 	}
 
