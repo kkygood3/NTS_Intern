c9feef1674771c35513d6461fbd84f0262f30dd2
siyoon <c.yun@nts-corp.com>
2019-01-23 15:06:18 +0900

    database 수정

--- src/main/java/com/nts/database/DBConnection.java
+++ src/main/java/com/nts/database/DBConnection.java
@@ -11,9 +11,9 @@ public abstract class DBConnection implements java.lang.AutoCloseable {
 
 	public static Connection getConnection() throws SQLException, ClassNotFoundException {
 		if (dbConnection == null) {
-			final String DB_URL = "jdbc:mysql://localhost:3306/testdb?serverTimezone=Asia/Seoul&useUnicode=true&characterEncoding=utf8";
-			final String DB_USER = "root";
-			final String DB_PASSWORD = "admin";
+			final String DB_URL = "jdbc:mysql://10.113.116.52:13306/user6?serverTimezone=Asia/Seoul&useUnicode=true&characterEncoding=utf8";
+			final String DB_USER = "user6";
+			final String DB_PASSWORD = "user6";
 			Class.forName("com.mysql.jdbc.Driver");
 			dbConnection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 		}
