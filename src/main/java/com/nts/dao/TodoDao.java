4149b7ed8e951845bbb1a1df5e913226a5fd6f41
siyoon <c.yun@nts-corp.com>
2019-01-23 16:45:37 +0900

    DBConnection 클래스 -> getConnection 메소드로 변경

--- src/main/java/com/nts/dao/TodoDao.java
+++ src/main/java/com/nts/dao/TodoDao.java
@@ -1,19 +1,23 @@
 package com.nts.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.nts.Const;
-import com.nts.database.DBConnection;
 import com.nts.database.DBQuery;
 import com.nts.dto.TodoDto;
 
 public class TodoDao {
+	private static Connection dbConnection;
+	
 	public int addTodo(TodoDto todo) {
 		int result = 0;
-		try (PreparedStatement ps = DBConnection.getConnection().prepareStatement(DBQuery.INSERT_SQL)) {
+		try (PreparedStatement ps = getConnection().prepareStatement(DBQuery.INSERT_SQL)) {
 			ps.setString(1, todo.getTitle());	
 			ps.setString(2, todo.getName());
 			ps.setInt(3, todo.getSequence());
@@ -26,7 +30,7 @@ public class TodoDao {
 
 	public List<TodoDto> getTodos() {
 		List<TodoDto> todos = new ArrayList<TodoDto>();
-		try (PreparedStatement ps = DBConnection.getConnection().prepareStatement(DBQuery.SELECT_SQL);
+		try (PreparedStatement ps = getConnection().prepareStatement(DBQuery.SELECT_SQL);
 			ResultSet rs = ps.executeQuery()) {
 			rs.beforeFirst();
 			while (rs.next()) {
@@ -45,7 +49,7 @@ public class TodoDao {
 
 	public int updateTodo(TodoDto todo) {
 		int result = 0;
-		try (PreparedStatement ps = DBConnection.getConnection().prepareStatement(DBQuery.UPDATE_DQL);) {
+		try (PreparedStatement ps = getConnection().prepareStatement(DBQuery.UPDATE_DQL);) {
 			ps.setString(1, Const.DOING);
 			ps.setLong(2, todo.getId());
 			ps.setString(3, Const.TODO);
@@ -60,6 +64,19 @@ public class TodoDao {
 		return result;
 	}
 
+	
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+		if (dbConnection == null) {
+			final String DB_URL = "jdbc:mysql://10.113.116.52:13306/user6?serverTimezone=Asia/Seoul&useUnicode=true&characterEncoding=utf8";
+			final String DB_USER = "user6";
+			final String DB_PASSWORD = "user6";
+
+			Class.forName("com.mysql.jdbc.Driver");
+			dbConnection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+		}
+		return dbConnection;
+	}
+	
 	private static class Holder {
 		public static final TodoDao INSTANCE = new TodoDao();
 	}
