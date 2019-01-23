fe5d5d007618f217d2fcb93c41cfe06dd67f85cb
Yook Sung Ryul <yook0328@gmail.com>
2019-01-23 11:00:36 +0900

    일부 수정

--- src/main/java/com/nts/jdbc/dto/TodoDto.java
+++ src/main/java/com/nts/jdbc/dto/TodoDto.java
@@ -29,57 +29,33 @@ public class TodoDto {
 		return id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
-
 	public String getTitle() {
 		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public int getSequence() {
 		return sequence;
-	}
-
-	public void setSequence(int sequence) {
-		this.sequence = sequence;
 	}
 
 	public String getType() {
 		return type;
 	}
 
-	public void setType(String type) {
-		this.type = type;
-	}
-
 	public String getRegDate() {
 		return regDate;
 	}
 
-	public void setRegDate(String regDate) {
-		this.regDate = regDate;
-	}
-
 	public static class Builder {
-		private int id;
-		private String title;
-		private String name;
-		private int sequence;
-		private String type;
-		private String regDate;
+		private int id = -1;
+		private String title = "";
+		private String name = "";
+		private int sequence = -1;
+		private String type = "";
+		private String regDate = "";
 
 		public Builder() {}
 
