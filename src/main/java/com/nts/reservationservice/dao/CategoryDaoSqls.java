9648254714f41ad8de0a47ebd17db3be6ef60db1
Yook Sung Ryul <yook0328@gmail.com>
2019-01-24 18:17:13 +0900

    CategoryDao 제작

---
+++ src/main/java/com/nts/reservationservice/dao/CategoryDaoSqls.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright 2019 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.nts.reservationservice.dao;
+
+public class CategoryDaoSqls {
+	public static final String SELECT_CATEGORY = "SELECT id, name FROM category ORDER BY id";
+}
