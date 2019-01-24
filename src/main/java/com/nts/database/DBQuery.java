3320582117ad4048ecf13d4891ed81493afa027a
siyoon <c.yun@nts-corp.com>
2019-01-24 10:07:27 +0900

    사용하지 않는 import 삭제

--- src/main/java/com/nts/database/DBQuery.java
+++ src/main/java/com/nts/database/DBQuery.java
@@ -1,8 +1,5 @@
 package com.nts.database;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 public class DBQuery {
 	public static final String INSERT_SQL = "insert into todo(title, name, sequence) values(?, ?, ?)";
 	public static final String SELECT_SQL = "select id, title, name, sequence, type, DATE_FORMAT(regdate, '%Y.%m.%d') as regdate from todo";
