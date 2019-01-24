d8831fe4d03476d5149bc9de4e4ab32634f28533
Yook Sung Ryul <yook0328@gmail.com>
2019-01-24 15:20:36 +0900

    mainPage.jsp 추가

--- src/main/java/com/nts/reservationservice/config/WebMvcContextConfiguration.java
+++ src/main/java/com/nts/reservationservice/config/WebMvcContextConfiguration.java
@@ -35,7 +35,7 @@ public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
 	@Override
 	public void addViewControllers(final ViewControllerRegistry registry) {
 		System.out.println("addViewControllers가 호출됩니다. ");
-		registry.addViewController("/").setViewName("mainpage");
+		registry.addViewController("/").setViewName("mainPage");
 	}
 
 	@Bean
