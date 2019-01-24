91d16fa2677a0a4336887c3ee40e21fa0d799e34
Yook Sung Ryul <yook0328@gmail.com>
2019-01-24 14:55:40 +0900

    WebApplicationInitializer 구축

---
+++ src/main/java/com/nts/reservationservice/config/WebApplicationInitializer.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2019 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.nts.reservationservice.config;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebApplicationInitializer implements org.springframework.web.WebApplicationInitializer {
+
+	private static final String DISPATCHER_SERVLET_NAME = "dispatcher";
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		FilterRegistration charEncodingFilterReg = servletContext.addFilter("CharacterEncodingFilter",
+			CharacterEncodingFilter.class);
+		charEncodingFilterReg.setInitParameter("encoding", "UTF-8");
+		charEncodingFilterReg.setInitParameter("forceEncoding", "true");
+		charEncodingFilterReg.addMappingForUrlPatterns(null, true, "/*");
+		registerDispatcherServlet(servletContext);
+	}
+
+	private void registerDispatcherServlet(ServletContext servletContext) {
+		AnnotationConfigWebApplicationContext dispatcherContext = createContext(WebMvcContextConfiguration.class);
+		ServletRegistration.Dynamic dispatcher;
+		dispatcher = servletContext.addServlet(DISPATCHER_SERVLET_NAME, new DispatcherServlet(dispatcherContext));
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("/");
+	}
+
+	private AnnotationConfigWebApplicationContext createContext(final Class<?>... annotatedClasses) {
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.register(annotatedClasses);
+		context.setConfigLocation("com.nts.guestbook.config");
+		return context;
+	}
+
+}
