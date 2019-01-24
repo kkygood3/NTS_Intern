f6dfb9f809a3452a0b90754587939dea4e4bc407
Yook Sung Ryul <yook0328@gmail.com>
2019-01-24 18:17:03 +0900

    PromotionDto CategoryDto 제작

---
+++ src/main/java/com/nts/reservationservice/dto/CategoryDto.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright 2019 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.nts.reservationservice.dto;
+
+public class CategoryDto {
+	int id;
+	String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "CategoryDto [id=" + id + ", name=" + name + "]";
+	}
+}
---
+++ src/main/java/com/nts/reservationservice/dto/PromotionDto.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright 2019 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.nts.reservationservice.dto;
+
+public class PromotionDto {
+	long id;
+	long productId;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(long productId) {
+		this.productId = productId;
+	}
+
+	@Override
+	public String toString() {
+		return "PromotionDto [id=" + id + ", productId=" + productId + "]";
+	}
+}
