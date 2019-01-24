2438bedd63b95f66f30c0280c95f49b13b4a8eb8
Yook Sung Ryul <yook0328@gmail.com>
2019-01-24 15:17:40 +0900

    ApplicationConfig 추가

---
+++ src/main/java/com/nts/reservationservice/config/ApplicationConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2019 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.nts.reservationservice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ComponentScan(basePackages = {"com.nts.reservationservice.dao", "com.nts.reservationservice.service"})
+@Import({DBConfig.class})
+public class ApplicationConfig {
+
+}
