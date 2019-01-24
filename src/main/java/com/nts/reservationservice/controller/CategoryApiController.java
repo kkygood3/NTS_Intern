e8159e906dbc0dc5d2e3d3dd24a81895ef7bbfed
Yook Sung Ryul <yook0328@gmail.com>
2019-01-24 18:42:31 +0900

    CategoryApiController 명세에 맞게 수정

--- src/main/java/com/nts/reservationservice/controller/CategoryApiController.java
+++ src/main/java/com/nts/reservationservice/controller/CategoryApiController.java
@@ -5,6 +5,7 @@
 
 package com.nts.reservationservice.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.nts.reservationservice.dto.CategoryDto;
 import com.nts.reservationservice.service.CategoryService;
+import com.nts.reservationservice.service.ProductService;
 
 @RestController
 @RequestMapping(path = "/api/categories")
@@ -24,13 +26,33 @@ public class CategoryApiController {
 	@Autowired
 	CategoryService categoryService;
 
+	@Autowired
+	ProductService productService;
+
 	@GetMapping
 	public Map<String, Object> list() {
 
 		Map<String, Object> map = new HashMap<>();
 		List<CategoryDto> list = categoryService.getCategorys();
 
-		map.put("items", list);
+		List<Map<String, Object>> items = new ArrayList();
+		for (CategoryDto item : list) {
+			int count = productService.getCount(item.getId());
+			Map<String, Object> combineItem = combineDataForGetResult(item, count);
+			items.add(combineItem);
+		}
+
+		map.put("items", items);
+		return map;
+	}
+
+	public Map<String, Object> combineDataForGetResult(CategoryDto category, int count) {
+		Map<String, Object> map = new HashMap();
+
+		map.put("count", count);
+		map.put("id", category.getId());
+		map.put("name", category.getName());
+
 		return map;
 	}
 }
