a8bc4880b13d4fd1c20ec1975baf1eedec5681cb
Yook Sung Ryul <yook0328@gmail.com>
2019-01-25 11:58:30 +0900

    ProductApiController 제작

--- src/main/java/com/nts/reservationservice/controller/ProductApiController.java
+++ src/main/java/com/nts/reservationservice/controller/ProductApiController.java
@@ -5,6 +5,7 @@
 
 package com.nts.reservationservice.controller;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -16,7 +17,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nts.reservationservice.dto.DisplayInfoDto;
+import com.nts.reservationservice.dto.FileInfoDto;
 import com.nts.reservationservice.dto.ProductDto;
+import com.nts.reservationservice.dto.ProductImageDto;
+import com.nts.reservationservice.service.DisplayInfoService;
+import com.nts.reservationservice.service.FileInfoService;
+import com.nts.reservationservice.service.ProductImageService;
 import com.nts.reservationservice.service.ProductService;
 
 @RestController
@@ -24,6 +31,12 @@ import com.nts.reservationservice.service.ProductService;
 public class ProductApiController {
 	@Autowired
 	ProductService productService;
+	@Autowired
+	DisplayInfoService displayInfoService;
+	@Autowired
+	ProductImageService productImageService;
+	@Autowired
+	FileInfoService fileInfoService;
 
 	@GetMapping
 	public Map<String, Object> list(@RequestParam(name = "start", required = false, defaultValue = "0") Integer start,
@@ -41,8 +54,29 @@ public class ProductApiController {
 			count = productService.getCount(categoryId);
 		}
 
-		map.put("items", list);
+		List<Map<String, Object>> items = new ArrayList();
+		for (ProductDto product : list) {
+			DisplayInfoDto displayInfo = displayInfoService.getDisplayInfoByProductId(product.getId());
+			ProductImageDto productImage = productImageService.getProductImageByProductIdAndType(product.getId(), "th");
+			FileInfoDto fileInfo = fileInfoService.getFileInfoById(productImage.getFileId());
+			Map<String, Object> item = combineDataForGetResult(product, displayInfo, fileInfo);
+			items.add(item);
+		}
+
+		map.put("items", items);
 		map.put("totalCount", count);
+		return map;
+	}
+
+	public Map<String, Object> combineDataForGetResult(ProductDto product, DisplayInfoDto displayInfo,
+		FileInfoDto fileInfo) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("displayInfoId", displayInfo.getId());
+		map.put("placeName", displayInfo.getPlaceName());
+		map.put("productContent", product.getContent());
+		map.put("productDescription", product.getDescription());
+		map.put("productId", product.getId());
+		map.put("productImageUrl", "../" + fileInfo.getSaveFileName());
 		return map;
 	}
 
