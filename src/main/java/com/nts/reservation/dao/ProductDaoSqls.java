a26f66aed1d445c1d86aeb8c311ed716ac58d6c3
이승수 <lee-seungsoo@nts-corp.com>
2019-01-28 10:31:59 +0900

    프로모션 sql 작성

--- src/main/java/com/nts/reservation/dao/ProductDaoSqls.java
+++ src/main/java/com/nts/reservation/dao/ProductDaoSqls.java
@@ -10,15 +10,20 @@ public class ProductDaoSqls {
 		+ "and product.id = product_image.product_id "
 		+ "and file_id = file_info.id "
 		+ "and type = 'th' ";
-	public static final String LIMIT = "limit :start, 4 ";
-	public static final String SELECT_COUNT_ALL = "SELECT count(*) FROM " + PRODUCT_TABLE;
-	public static final String COUNT_BY_CATEGORY_ID = SELECT_COUNT_ALL + "and category_id = :categoryId";
-	public static final String GET_PRODUCTS = "SELECT display_info.id as display_info_id, "
+	private static final String PRODUCT_COULMN = "SELECT display_info.id as display_info_id, "
 		+ "place_name, "
 		+ "content as product_content, "
 		+ "description as product_description, "
 		+ "product.id as product_id, "
-		+ "save_file_name as product_image_url "
-		+ "FROM " + PRODUCT_TABLE;
+		+ "save_file_name as product_image_url ";
+
+	public static final String LIMIT = "limit :start, 4 ";
+
+	public static final String SELECT_COUNT_ALL = "SELECT count(*) FROM " + PRODUCT_TABLE;
+	public static final String COUNT_BY_CATEGORY_ID = SELECT_COUNT_ALL + "and category_id = :categoryId";
+
+	public static final String GET_PRODUCTS = PRODUCT_COULMN + "FROM " + PRODUCT_TABLE;
+	public static final String GET_PROMOTION_PRODUCTS = PRODUCT_COULMN + "FROM promotion, " + PRODUCT_TABLE;
+
 	public static final String GET_PRODUCTS_BY_CATEGORY_ID = GET_PRODUCTS + "and category_id = :categoryId ";
 }
