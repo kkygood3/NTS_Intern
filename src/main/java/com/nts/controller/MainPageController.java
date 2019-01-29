2abcf4b9f3945d2cd052b33389cafaf62f88c83c
박우성 <woosung.park@nts-corp.com>
2019-01-29 17:23:54 +0900

    주어진 템플릿을 바탕으로 메인화면 js 작성

--- src/main/java/com/nts/controller/MainPageController.java
+++ src/main/java/com/nts/controller/MainPageController.java
@@ -1,15 +1,11 @@
 package com.nts.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.nts.dto.Product;
@@ -34,6 +30,6 @@ public class MainPageController {
 		model.addAttribute("list", list);
 		model.addAttribute("count", count);
 
-		return "main";
+		return "mainpage";
 	}
 }
