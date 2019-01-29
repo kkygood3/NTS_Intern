0739f3e74c5801e134d28e147ec18d58e40efd6d
박우성 <woosung.park@nts-corp.com>
2019-01-29 18:38:50 +0900

    404 Error redirect 처리

---
+++ src/main/java/com/nts/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.nts.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	@GetMapping("/mainpage")
+	public String mainPageVisit() {
+		return "mainpage";
+	}
+}
