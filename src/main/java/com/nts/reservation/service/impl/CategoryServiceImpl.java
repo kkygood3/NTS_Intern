8676ffaa651722a09dad17ccc1e97045b758c2c4
siyoon <c.yun@nts-corp.com>
2019-01-29 17:26:22 +0900

    어노테이션 추가

--- src/main/java/com/nts/reservation/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/nts/reservation/service/impl/CategoryServiceImpl.java
@@ -3,11 +3,13 @@ package com.nts.reservation.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.nts.reservation.dao.CategoryDao;
 import com.nts.reservation.dto.Category;
 import com.nts.reservation.service.CategoryService;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
 	@Autowired
