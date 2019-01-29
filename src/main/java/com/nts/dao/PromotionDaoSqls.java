3afed0acf2d3f66a5c35163c1a2e8fb9555963c0
배진우 <jinwoo.bae@nts-corp.com>
2019-01-29 16:42:24 +0900

    가독성 수정

--- src/main/java/com/nts/dao/PromotionDaoSqls.java
+++ src/main/java/com/nts/dao/PromotionDaoSqls.java
@@ -9,16 +9,16 @@ package com.nts.dao;
  *
  */
 public class PromotionDaoSqls {
-	public static final String SELECT_ALL = 
-		"SELECT " + 
-		"  promotion.id AS id, " + 
-		"  promotion.product_id AS productId, " + 
-		"  file_info.save_file_name AS productImageUrl " + 
-		"FROM promotion, " + 
-		"     product_image, " + 
-		"     file_info " + 
-		"WHERE promotion.product_id = product_image.product_id " + 
-		"AND product_image.type = 'th' " + 
-		"AND product_image.file_id = file_info.id " +
-		"ORDER BY promotion.id ASC";
+	public static final String SELECT_ALL = "SELECT "
+		+ " 	promotion.id AS id, "
+		+ " 	promotion.product_id AS productId, "
+		+ " 	file_info.save_file_name AS productImageUrl "
+		+ " FROM promotion, "
+		+ " 	product_image, "
+		+ " 	file_info "
+		+ " WHERE "
+		+ " 	promotion.product_id = product_image.product_id "
+		+ " 	AND product_image.type = 'th' "
+		+ " 	AND product_image.file_id = file_info.id "
+		+ " ORDER BY promotion.id ASC";
 }
