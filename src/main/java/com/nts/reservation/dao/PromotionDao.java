9a9384020ccde9965bfb5fa76910c204a5fc2a0c
siyoon <c.yun@nts-corp.com>
2019-01-29 17:05:24 +0900

    DB연결

--- src/main/java/com/nts/reservation/dao/PromotionDao.java
+++ src/main/java/com/nts/reservation/dao/PromotionDao.java
@@ -8,9 +8,11 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.nts.reservation.dto.Promotion;
 
+@Repository
 public class PromotionDao {
 	private NamedParameterJdbcTemplate jdbc;
 
@@ -18,7 +20,7 @@ public class PromotionDao {
 		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
 	}
 
-	public List<Promotion> selectAll(Integer start, Integer limit) {
+	public List<Promotion> selectAll() {
 
 		return jdbc.queryForList(SELECT_SAVE_FILE_NAME, Collections.EMPTY_MAP);
 	}
