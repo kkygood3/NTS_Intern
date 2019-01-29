d5ea9ba2680f9cfafe50a06f21f9b4b3dac15c4f
Yook Sung Ryul <yook0328@gmail.com>
2019-01-29 12:24:34 +0900

    GROUP By 내에 category.id 명시

--- src/main/java/com/nts/reservation/dao/CategoryDaoSqls.java
+++ src/main/java/com/nts/reservation/dao/CategoryDaoSqls.java
@@ -10,5 +10,5 @@ package com.nts.reservation.dao;
  */
 public class CategoryDaoSqls {
 	public static final String SELECT_CATEGORY = "SELECT category.id, category.name, COUNT(*) AS count FROM category"
-		+ " INNER JOIN product ON category.id = product.category_id INNER JOIN display_info ON product.id = display_info.product_id GROUP BY category.name ORDER BY id";
+		+ " INNER JOIN product ON category.id = product.category_id INNER JOIN display_info ON product.id = display_info.product_id GROUP BY category.name, category.id ORDER BY id";
 }
