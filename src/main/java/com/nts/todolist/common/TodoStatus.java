e45a8d64883c700382420217ca6d473e6539ccce
yongjoon.Park <yongjoon.Park@nts-corp.com>
2019-01-31 10:55:34 +0900

    if문 제거

--- src/main/java/com/nts/todolist/common/TodoStatus.java
+++ src/main/java/com/nts/todolist/common/TodoStatus.java
@@ -4,17 +4,34 @@
  */
 package com.nts.todolist.common;
 
+import java.util.Arrays;
+
 /**
  * Todo의 type에 대한 enum class
  * @author yongjoon.Park
  */
 public enum TodoStatus {
 
-	TODO("TODO"),
-	DOING("DOING"),
-	DONE("DONE");
+	TODO("TODO") {
+		@Override
+		public TodoStatus getNextStatus() {
+			return DOING;
+		}
+	},
+	DOING("DOING") {
+		@Override
+		public TodoStatus getNextStatus() {
+			return DONE;
+		}
+	},
+	DONE("DONE") {
+		@Override
+		public TodoStatus getNextStatus() {
+			return DONE;
+		}
+	};
 
-	private String todoStatus;
+	private final String todoStatus;
 
 	private TodoStatus(String todoStatus) {
 		this.todoStatus = todoStatus;
@@ -24,24 +41,18 @@ public enum TodoStatus {
 		return todoStatus;
 	}
 
-	/**현재 todoStatus값을 다음 단계의 todoStatus으로 반환
-	 * @author yongjoon.Park
-	 */
-	public TodoStatus getNextStatus() {
-		return TodoStatus.values()[this.ordinal() + 1];
-	}
-
 	/**입력 받은 값이 TodoStatus Enum에 존재하는 지 확인
 	 * @author yongjoon.Park
 	 * @param type 해당 enum에 존재하는 지 확인할 type값
 	 * @return 해당 type이 있을 경우 true, 없을 경우 false를 return한다.
 	 */
 	public static boolean isExistTodoStatus(String type) {
-		for (TodoStatus compareStatus : TodoStatus.values()) {
-			if (compareStatus.getTodoStatus().equals(type)) {
-				return true;
-			}
-		}
-		return false;
+		return Arrays.asList(TodoStatus.values()).stream()
+			.anyMatch(todoStatus -> todoStatus.getTodoStatus().equals(type));
 	}
+	
+	/**현재 todoStatus값을 다음 단계의 todoStatus으로 반환
+	 * @author yongjoon.Park
+	 */
+	public abstract TodoStatus getNextStatus();
 }
