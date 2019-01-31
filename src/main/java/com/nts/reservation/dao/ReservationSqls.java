5d06412fcb414a6a3d9f32c9c35a93f3c6929a20
이승수 <lee-seungsoo@nts-corp.com>
2019-01-31 10:53:05 +0900

    sql 수정

--- src/main/java/com/nts/reservation/dao/ReservationSqls.java
+++ src/main/java/com/nts/reservation/dao/ReservationSqls.java
@@ -15,34 +15,32 @@ public class ReservationSqls {
 		+ "ON product.id = product_image.product_id "
 		+ "INNER JOIN file_info "
 		+ "ON file_id = file_info.id ";
-	private static final String PRODUCT_COULMN = "display_info.id as display_info_id, "
-		+ "place_name, "
-		+ "content as product_content, "
-		+ "description as product_description, "
-		+ "product.id as product_id, "
-		+ "save_file_name as product_image_url ";
+	private static final String PRODUCT_COULMN = "display_info.id AS display_info_id"
+		+ ", place_name"
+		+ ", content AS product_content"
+		+ ", description AS product_description"
+		+ ", product.id AS product_id"
+		+ ", save_file_name AS product_image_url ";
 
-	public static final String LIMIT = "limit :start, 4 ";
+	public static final String LIMIT = "LIMIT :start, 4 ";
 
-	public static final String SELECT_COUNT_ALL = "SELECT count(*) FROM " + PRODUCT_TABLE
-		+ "WHERE type = 'th'";
-	public static final String COUNT_BY_CATEGORY_ID = "SELECT count(*) FROM " + PRODUCT_TABLE
-		+ "WHERE type = 'th' "
-		+ "and category_id = :categoryId ";
+	public static final String SELECT_COUNT_ALL = "SELECT count(*) FROM " + PRODUCT_TABLE + "WHERE type = 'th'";
+	public static final String COUNT_BY_CATEGORY_ID = SELECT_COUNT_ALL + "AND category_id = :categoryId ";
 
 	public static final String GET_PRODUCTS = "SELECT " + PRODUCT_COULMN + "FROM " + PRODUCT_TABLE
-		+ "GROUP BY display_info.id ";
-	public static final String GET_PRODUCTS_BY_CATEGORY_ID = "SELECT " + PRODUCT_COULMN + "FROM " + PRODUCT_TABLE
-		+ "WHERE category_id = :categoryId "
-		+ "GROUP BY display_info.id ";
+		+ "WHERE type = 'th'";
+	public static final String GET_PRODUCTS_BY_CATEGORY_ID = GET_PRODUCTS + "AND category_id = :categoryId ";
 
 	public static final String GET_PROMOTION_PRODUCTS = "SELECT " + PRODUCT_COULMN + "FROM " + PRODUCT_TABLE
 		+ "INNER JOIN promotion "
 		+ "ON promotion.product_id = product.id "
-		+ "GROUP BY display_info.id ";
-	public static final String COUNT_PROMOTION = "SELECT count(*) FROM promotion";
+		+ "WHERE type = 'th'";
+	public static final String COUNT_PROMOTION = "SELECT count(*) FROM " + PRODUCT_TABLE
+		+ "INNER JOIN promotion "
+		+ "ON promotion.product_id = product.id "
+		+ "WHERE type = 'th'";
 
-	public static final String GET_CATEGORIES = "SELECT category.id, name, count(*) as count "
+	public static final String GET_CATEGORIES = "SELECT category.id, name, count(*) AS count "
 		+ "FROM category "
 		+ "INNER JOIN product "
 		+ "ON category.id = product.category_id "
