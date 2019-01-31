c94f2ff2d79498b7af4e33f5d01daf64bddce8a1
Yook Sung Ryul <yook0328@gmail.com>
2019-01-31 10:46:04 +0900

    SQL 문 일부 수정.

--- src/main/java/com/nts/reservation/dao/CategoryDaoSqls.java
+++ src/main/java/com/nts/reservation/dao/CategoryDaoSqls.java
@@ -9,6 +9,10 @@ package com.nts.reservation.dao;
  * @author 육성렬
  */
 public class CategoryDaoSqls {
-	public static final String SELECT_CATEGORY = "SELECT category.id, category.name, COUNT(*) AS count FROM category"
-		+ " INNER JOIN product ON category.id = product.category_id INNER JOIN display_info ON product.id = display_info.product_id GROUP BY category.name, category.id ORDER BY id";
+	public static final String SELECT_CATEGORY = "SELECT category.id, category.name, COUNT(*) AS count"
+		+ " FROM category"
+		+ " INNER JOIN product ON category.id = product.category_id"
+		+ " INNER JOIN display_info ON product.id = display_info.product_id"
+		+ " GROUP BY category.id, category.name"
+		+ " ORDER BY category.id";
 }
