a842982da1276d0cf1bdaed0dd45f54d67af310f
Yook Sung Ryul <yook0328@gmail.com>
2019-01-31 10:59:32 +0900

    함수명 및 foreach로 수정

--- src/main/webapp/resources/js/util.js
+++ src/main/webapp/resources/js/util.js
@@ -1,24 +1,26 @@
-
 /**
  * @function sendGet 서버에 Get 메서드 요청 함수.
- * @param {String} path 요청 URL
- * @param {JSON} params  
- * @param {Function} onCallback 콜백 함수.
+ * @param {String}
+ *            path 요청 URL
+ * @param {JSON}
+ *            params value 값에 Array 형태 넣지말것.
+ * @param {Function}
+ *            onCallback 콜백 함수.
  */
-function sendGet (path, params, onCallback) {
-    var data = "";
-    if(params){
-        data = Object.keys(params).map(function(key){
-            return key + "=" + encodeURIComponent(params[key]);
-        }).join("&");
-    }
-    var request = new XMLHttpRequest();
-    request.addEventListener("load", function(event){
-        onCallback(event.target);
-    });
+function sendGet(path, params, onCallback) {
+	var data = "";
+	if (params) {
+		data = Object.keys(params).map(function(key) {
+			return key + "=" + encodeURIComponent(params[key]);
+		}).join("&");
+	}
+	var request = new XMLHttpRequest();
+	request.addEventListener("load", function(event) {
+		onCallback(event.target);
+	});
 
-    var url = path + (data.length == 0 ? "" : "?") + data;
-    request.open("GET", url);
-    request.setRequestHeader("Content-type","charset=utf-8");
-    request.send();
+	var url = path + (data.length == 0 ? "" : "?") + data;
+	request.open("GET", url);
+	request.setRequestHeader("Content-type", "charset=utf-8");
+	request.send();
 }
