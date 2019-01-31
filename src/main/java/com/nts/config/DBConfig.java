fe45c092b5cd3067e744d18c445f072f367aad20
박우성 <woosung.park@nts-corp.com>
2019-01-31 10:13:16 +0900

    DB 접속정보를 application.properties에 저장

--- src/main/java/com/nts/config/DBConfig.java
+++ src/main/java/com/nts/config/DBConfig.java
@@ -7,25 +7,38 @@ package com.nts.config;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
-import com.nts.constant.DBAccount;
-
 @Configuration
 @EnableTransactionManagement
+@PropertySource("classpath:application.properties")
 public class DBConfig implements TransactionManagementConfigurer {
+	@Value("${spring.datasource.driver-class-name}")
+	private String driverClassName;
+	
+	@Value("${spring.datasource.url}")
+	private String url;
+	
+	@Value("${spring.datasource.username}")
+	private String userName;
+	
+	@Value("${spring.datasource.password}")
+	private String password;
+	
 	@Bean
 	public DataSource dataSource() {
 		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName(DBAccount.DRIVER_CLASS_NAME);
-		dataSource.setUrl(DBAccount.URL);
-		dataSource.setUsername(DBAccount.USERNAME);
-		dataSource.setPassword(DBAccount.PASSWORD);
+		dataSource.setDriverClassName(driverClassName);
+		dataSource.setUrl(url);
+		dataSource.setUsername(userName);
+		dataSource.setPassword(password);
 		return dataSource;
 	}
 
