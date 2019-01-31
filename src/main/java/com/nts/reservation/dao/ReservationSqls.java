6288fcd783174c8194668d1b3e72ed7c41b37274
이승수 <lee-seungsoo@nts-corp.com>
2019-01-31 13:19:29 +0900

    table에 alias추가

--- src/main/java/com/nts/reservation/dao/ReservationSqls.java
+++ src/main/java/com/nts/reservation/dao/ReservationSqls.java
@@ -8,43 +8,43 @@ package com.nts.reservation.dao;
 * @author  : 이승수
 */
 public class ReservationSqls {
-	private static final String PRODUCT_TABLE = "product "
-		+ "INNER JOIN display_info "
-		+ "ON product.id = display_info.product_id "
-		+ "INNER JOIN product_image "
-		+ "ON product.id = product_image.product_id "
-		+ "INNER JOIN file_info "
-		+ "ON file_id = file_info.id ";
-	private static final String PRODUCT_COULMN = "display_info.id AS display_info_id"
-		+ ", place_name"
-		+ ", content AS product_content"
-		+ ", description AS product_description"
-		+ ", product.id AS product_id"
-		+ ", save_file_name AS product_image_url ";
+	private static final String PRODUCT_TABLE = "product as p "
+		+ "INNER JOIN display_info as d "
+		+ "ON p.id = d.product_id "
+		+ "INNER JOIN product_image as i "
+		+ "ON p.id = i.product_id "
+		+ "INNER JOIN file_info as f "
+		+ "ON i.file_id = f.id ";
+	private static final String PRODUCT_COULMN = "d.id AS display_info_id"
+		+ ", d.place_name "
+		+ ", p.content AS product_content"
+		+ ", p.description AS product_description"
+		+ ", p.id AS product_id"
+		+ ", f.save_file_name AS product_image_url ";
 
 	public static final String LIMIT = "LIMIT :start, :limit ";
 
-	public static final String SELECT_COUNT_ALL = "SELECT count(*) FROM " + PRODUCT_TABLE + "WHERE type = 'th'";
-	public static final String COUNT_BY_CATEGORY_ID = SELECT_COUNT_ALL + "AND category_id = :categoryId ";
+	public static final String SELECT_COUNT_ALL = "SELECT count(*) FROM " + PRODUCT_TABLE + "WHERE i.type = 'th'";
+	public static final String COUNT_BY_CATEGORY_ID = SELECT_COUNT_ALL + "AND p.category_id = :categoryId ";
 
 	public static final String GET_PRODUCTS = "SELECT " + PRODUCT_COULMN + "FROM " + PRODUCT_TABLE
-		+ "WHERE type = 'th'";
-	public static final String GET_PRODUCTS_BY_CATEGORY_ID = GET_PRODUCTS + "AND category_id = :categoryId ";
+		+ "WHERE i.type = 'th'";
+	public static final String GET_PRODUCTS_BY_CATEGORY_ID = GET_PRODUCTS + "AND p.category_id = :categoryId ";
 
 	public static final String GET_PROMOTION_PRODUCTS = "SELECT " + PRODUCT_COULMN + "FROM " + PRODUCT_TABLE
-		+ "INNER JOIN promotion "
-		+ "ON promotion.product_id = product.id "
-		+ "WHERE type = 'th'";
+		+ "INNER JOIN promotion as pr "
+		+ "ON pr.product_id = p.id "
+		+ "WHERE i.type = 'th'";
 	public static final String COUNT_PROMOTION = "SELECT count(*) FROM " + PRODUCT_TABLE
-		+ "INNER JOIN promotion "
-		+ "ON promotion.product_id = product.id "
-		+ "WHERE type = 'th'";
+		+ "INNER JOIN promotion as pr "
+		+ "ON pr.product_id = p.id "
+		+ "WHERE i.type = 'th'";
 
-	public static final String GET_CATEGORIES = "SELECT category.id, name, count(*) AS count "
-		+ "FROM category "
-		+ "INNER JOIN product "
-		+ "ON category.id = product.category_id "
-		+ "INNER JOIN display_info "
-		+ "ON product.id = display_info.product_id "
-		+ "GROUP BY category.id ";
+	public static final String GET_CATEGORIES = "SELECT c.id, c.name, count(*) AS count "
+		+ "FROM category as c "
+		+ "INNER JOIN product as p "
+		+ "ON c.id = p.category_id "
+		+ "INNER JOIN display_info as d "
+		+ "ON p.id = d.product_id "
+		+ "GROUP BY c.id ";
 }
