75a1d1a1cc3fd273eae1503897f0453e2348fb62
USER <USER@AL01288279>
2019-02-05 21:57:58 +0900

    SQL literal variable name 수정

--- src/main/java/com/nts/reservation/dao/sql/PromotionDaoSqls.java
+++ src/main/java/com/nts/reservation/dao/sql/PromotionDaoSqls.java
@@ -1,7 +1,7 @@
 package com.nts.reservation.dao.sql;
 
 public class PromotionDaoSqls {
-	public static final String SELECT_SAVE_FILE_NAMES_IN_PROMOTION = 
+	public static final String SELECT_MA_TYPE_SAVE_FILE_NAMES_IN_PROMOTION = 
 		"SELECT save_file_name FROM file_info WHERE id IN ("
 		+ "	SELECT file_id FROM product_image WHERE type = 'ma' AND product_id IN ("
 		+ "		SELECT id FROM product  WHERE id IN ("
