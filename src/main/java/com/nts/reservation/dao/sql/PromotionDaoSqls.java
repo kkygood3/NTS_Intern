acc28df7e7da9c69a1a6d91029a75d450b125995
USER <USER@AL01288279>
2019-02-05 21:52:46 +0900

    쿼리문 뒤 세미콜론 삭제

--- src/main/java/com/nts/reservation/dao/sql/PromotionDaoSqls.java
+++ src/main/java/com/nts/reservation/dao/sql/PromotionDaoSqls.java
@@ -5,5 +5,5 @@ public class PromotionDaoSqls {
 		"SELECT save_file_name FROM file_info WHERE id IN ("
 		+ "	SELECT file_id FROM product_image WHERE type = 'ma' AND product_id IN ("
 		+ "		SELECT id FROM product  WHERE id IN ("
-		+ "			SELECT product_id FROM promotion)));";
+		+ "			SELECT product_id FROM promotion)))";
 }
