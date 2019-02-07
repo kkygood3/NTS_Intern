921055767ccc1272d9e3cc36158efaecec79800d
yongjoon.park <USER@AD01031808>
2019-02-07 18:45:20 +0900

    MainController 작성 (임시)

--- src/main/java/com/nts/reservation/controller/MainController.java
+++ src/main/java/com/nts/reservation/controller/MainController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.nts.reservation.dto.Category;
 import com.nts.reservation.dto.Product;
+import com.nts.reservation.dto.Promotion;
 import com.nts.reservation.service.CategoryService;
 import com.nts.reservation.service.ProductService;
+import com.nts.reservation.service.PromotionService;
 
 @Controller
 public class MainController {
@@ -20,16 +22,23 @@ public class MainController {
 
 	@Autowired
 	CategoryService categoryService;
+	
+	@Autowired
+	PromotionService promotionService;
 
 	@GetMapping(path = "/main")
 	public String goMainPage(ModelMap model) {
+		// TODO 전부 AJAX 처리
 		List<Product> productList = productService.getProductsByCategory(0, 0);
 		int productCount = productService.getProductsCountByCategory(0);
 		List<Category> categoryList = categoryService.getCategory();
+		
+		List<Promotion> promotionList = promotionService.getPromotions();
 
 		model.addAttribute("productList", productList);
 		model.addAttribute("productCount", productCount);
 		model.addAttribute("categoryList", categoryList);
+		model.addAttribute("promotionList", promotionList);
 		return "mainpage";
 	}
 }
