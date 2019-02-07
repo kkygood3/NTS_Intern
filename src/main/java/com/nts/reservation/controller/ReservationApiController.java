243c8f9ff43932167c29b4341156bce13865b1be
이승수 <lee-seungsoo@nts-corp.com>
2019-02-07 09:50:26 +0900

    api controller 업데이트

--- src/main/java/com/nts/reservation/controller/ReservationApiController.java
+++ src/main/java/com/nts/reservation/controller/ReservationApiController.java
@@ -4,18 +4,25 @@
  */
 package com.nts.reservation.controller;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nts.reservation.dto.Category;
+import com.nts.reservation.dto.Comment;
+import com.nts.reservation.dto.DisplayInfo;
+import com.nts.reservation.dto.DisplayInfoImage;
 import com.nts.reservation.dto.Product;
+import com.nts.reservation.dto.ProductImage;
+import com.nts.reservation.dto.ProductPrice;
 import com.nts.reservation.service.CategoryService;
 import com.nts.reservation.service.ProductService;
 
@@ -45,6 +52,26 @@ public class ReservationApiController {
 		Map<String, Object> map = new HashMap<>();
 		map.put("items", products);
 		map.put("totalCount", countProducts);
+
+		return map;
+	}
+
+	@GetMapping(path = "/products/{displayInfoId}")
+	public Map<String, Object> getDetail(@PathVariable("displayInfoId") Integer displayInfoId) {
+		BigDecimal averageScore = detailProductService.getAverageScore(displayInfoId);
+		List<Comment> comments = detailProductService.getComments(displayInfoId);
+		DisplayInfo displayInfo = detailProductService.getDisplayInfo(displayInfoId);
+		DisplayInfoImage displayInfoImage = detailProductService.getDisplayInfoImage(displayInfoId);
+		List<ProductImage> productImages = detailProductService.getProductImages(displayInfoId);
+		List<ProductPrice> productPrices = detailProductService.getProductPrices(displayInfoId);
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("averageScore", averageScore);
+		map.put("comments", comments);
+		map.put("displayInfo", displayInfo);
+		map.put("displayInfoImage", displayInfoImage);
+		map.put("productImages", productImages);
+		map.put("productPrices", productPrices);
 
 		return map;
 	}
