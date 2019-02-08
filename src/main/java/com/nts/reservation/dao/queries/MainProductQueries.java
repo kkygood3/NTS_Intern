c0e2213102672688f14afbd6cf7eeed69c8c2f71
김기용 <giyong.kim@nts-corp.com>
2019-02-08 11:00:44 +0900

    MainProductQuery문 가독성을 위해 문법 정리

--- src/main/java/com/nts/reservation/dao/queries/MainProductQueries.java
+++ src/main/java/com/nts/reservation/dao/queries/MainProductQueries.java
@@ -6,31 +6,29 @@
 package com.nts.reservation.dao.queries;
 
 public class MainProductQueries {
-	public static final String SELECT_PROUDUCT_PAGE =
-		"SELECT display_info.id AS display_info_id, display_info.place_name, product.content AS product_content, product.description AS product_description, product.id AS product_id, file_info.save_file_name AS product_image_url\r\n" +
+	public static final String SELECT_PROUDUCT_PAGE = "SELECT display_info.id AS display_info_id, display_info.place_name, product.content AS product_content, product.description AS product_description, product.id AS product_id, file_info.save_file_name AS product_image_url\r\n"
+		+
 		"FROM product\r\n" +
 		"INNER JOIN product_image ON product.id = product_image.product_id\r\n" +
 		"INNER JOIN display_info ON product.id = display_info.product_id\r\n" +
 		"INNER JOIN file_info ON product_image.file_id = file_info.id and product_image.type='th'\r\n" +
 		"LIMIT :start, :limit;";
 
-	public static final String SELECT_PROUDUCT_PAGE_BY_CATEGORY =
-		"SELECT display_info.id AS display_info_id, display_info.place_name, product.content AS product_content, product.description AS product_description, product.id AS product_id, file_info.save_file_name AS product_image_url\r\n" +
+	public static final String SELECT_PROUDUCT_PAGE_BY_CATEGORY = "SELECT display_info.id AS display_info_id, display_info.place_name, product.content AS product_content, product.description AS product_description, product.id AS product_id, file_info.save_file_name AS product_image_url\r\n"
+		+
 		"FROM product\r\n" +
 		"INNER JOIN product_image ON product.id = product_image.product_id\r\n" +
 		"INNER JOIN display_info ON product.id = display_info.product_id\r\n" +
 		"INNER JOIN file_info ON product_image.file_id = file_info.id and product_image.type='th'\r\n" +
-			"WHERE category_id = :categoryId\r\n" +
+		"WHERE category_id = :categoryId\r\n" +
 		"LIMIT :start, :limit;";
 
-	public static final String SELECT_PRODUCT_COUNT =
-		"SELECT COUNT(display_info.id) FROM product" +
-		"INNER JOIN product_image ON product.id = product_image.product_id and product_image.type = 'th'" +
+	public static final String SELECT_PRODUCT_COUNT = "SELECT COUNT(display_info.id) FROM product\r\n" +
+		"INNER JOIN product_image ON product.id = product_image.product_id and product_image.type = 'th'\r\n" +
 		"INNER JOIN display_info ON product.id = display_info.product_id";
 
-	public static final String SELECT_PRODUCT_COUNT_BY_CATEGORY =
-		"SELECT COUNT(display_info.id) FROM product" +
-		"INNER JOIN product_image ON product.id = product_image.product_id and product_image.type = 'th'" +
-		"INNER JOIN display_info ON product.id = display_info.product_id" +
+	public static final String SELECT_PRODUCT_COUNT_BY_CATEGORY = "SELECT COUNT(display_info.id) FROM product\r\n" +
+		"INNER JOIN product_image ON product.id = product_image.product_id and product_image.type = 'th'\r\n" +
+		"INNER JOIN display_info ON product.id = display_info.product_id\r\n" +
 		"WHERE category_id = :categoryId";
 }
