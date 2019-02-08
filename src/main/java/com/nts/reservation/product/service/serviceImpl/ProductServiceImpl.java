36bb5e1b5e24e883a5e4a0d0031e2b7a96cdef99
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-08 18:34:46 +0900

    hotfix

--- src/main/java/com/nts/reservation/product/service/serviceImpl/ProductServiceImpl.java
+++ src/main/java/com/nts/reservation/product/service/serviceImpl/ProductServiceImpl.java
@@ -4,13 +4,10 @@
  */
 package com.nts.reservation.product.service.serviceImpl;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nts.reservation.product.dao.ProductDao;
-import com.nts.reservation.product.dto.Product;
 import com.nts.reservation.product.dto.ProductResponse;
 import com.nts.reservation.product.service.ProductService;
 
@@ -22,10 +19,9 @@ public class ProductServiceImpl implements ProductService {
 
 	@Override
 	public ProductResponse getProductsByCategory(int categoryId, int start) {
-		List<Product> products = productDao.getProductsByCategory(categoryId, start, LIMIT);
 		return ProductResponse.builder()
-			.items(products)
-			.totalCount(products.size())
+			.items(productDao.getProductsByCategory(categoryId, start, LIMIT))
+			.totalCount(productDao.getProductCountByCategory(categoryId))
 			.build();
 	}
 }
