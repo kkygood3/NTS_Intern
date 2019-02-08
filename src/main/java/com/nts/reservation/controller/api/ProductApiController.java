8f170dd11870297477e7fe63bc0e7339cde53147
박우성 <woosung.park@nts-corp.com>
2019-02-08 10:28:08 +0900

    Api 요청을 각자 다른 Class에서 받도록 변경

---
+++ src/main/java/com/nts/reservation/controller/api/ProductApiController.java
@@ -0,0 +1,45 @@
+package com.nts.reservation.controller.api;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nts.reservation.dto.MainProduct;
+import com.nts.reservation.service.MainProductService;
+
+@RestController
+public class ProductApiController {
+	@Autowired
+	private MainProductService mainProductService;
+	
+	/**
+	 * /products 요청을 받아 메인 페이지에 상품정보를 출력
+	 * @param	categoryId	해당 카테고리에 속하는 상품 요청. 0일때 카테고리 구분 없음
+	 * @param	start	페이지에 출력할 데이터의 시작 index
+	 * @return	JSON text
+	 */
+	@GetMapping("/api/products")
+	public Map<String, Object> products(
+		@RequestParam(name = "categoryId", required = false, defaultValue = "0") Integer categoryId,
+		@RequestParam(name = "start", required = false, defaultValue = "0") Integer start) {
+
+		List<MainProduct> productList = new ArrayList<>();
+		int totalCount = mainProductService.getCount(categoryId);
+
+		if (totalCount > 0) {
+			productList = mainProductService.getProducts(categoryId, start);
+		}
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("productList", productList);
+		map.put("totalCount", totalCount);
+
+		return map;
+	}
+}
