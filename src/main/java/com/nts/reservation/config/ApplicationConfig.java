149d987730377e5d66e7b07ebc163eb3db1a1247
박두익 <duik.park@nts-corp.com>
2019-02-08 10:45:30 +0900

    ApplicationConfig.java 수정 및 dao, service 패키지 추가

--- src/main/java/com/nts/reservation/config/ApplicationConfig.java
+++ src/main/java/com/nts/reservation/config/ApplicationConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Import;
  * @Author Duik Park, duik.park@nts-corp.com
  */
 @Configuration
-@ComponentScan(basePackages = {"kr.or.connect.guestbook.dao", "kr.or.connect.guestbook.service"})
+@ComponentScan(basePackages = {"com.nts.reservation.dao", "com.nts.reservation.service"})
 @Import({DbConfig.class})
 public class ApplicationConfig {
 
