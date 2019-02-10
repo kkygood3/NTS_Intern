da4e2a4c90e71aad48bad88ab1914ee8909a2ea1
USER <USER@AL01288279>
2019-02-11 01:10:51 +0900

    비주얼영역 etc이미지 가져오기

---
+++ src/main/webapp/javascript/detail/visual.js
@@ -0,0 +1,17 @@
+function sendGetEtcImageAjax() {
+	sendGETAjax("./file_info?product_id="+ displayInfo.productId + "&type=et", setVisualArea);
+}
+
+function setVisualArea(etcImage) {
+	var template = document.getElementById("visual_area").innerText;
+	var sectionVisual = document.getElementsByClassName("section_visual")[0];
+	template = template.replace("{image_length}", etcImage.length > 0 ? 2 : 1);
+	var bindTemplate = Handlebars.compile(template); 
+	if (etcImage.length > 0) {
+		etcImage = {"etcImage" : etcImage[0]};
+	} else {
+		etcImage = null;
+	}
+	var resultHTML = sectionVisual.innerHTML + bindTemplate(etcImage);
+	sectionVisual.innerHTML = resultHTML;
+}
