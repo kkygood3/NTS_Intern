4e0b5e61b5d27999ee948b5d62bd072146db6f55
임상현 <life4lord93@gmail.com>
2019-02-11 15:24:31 +0900

    template 처리하는 공통 js 개발

---
+++ src/main/webapp/resources/js/template_maker.js
@@ -0,0 +1,18 @@
+template = {
+	setting : function(){
+		var templateList = document.querySelectorAll(".template");
+		
+		templateList.forEach((item)=>{
+			var functionName = "parse" + item.dataset.parseName +"ToHtml";
+			var templateHtml = item.innerHTML;
+			
+			this[functionName] = Handlebars.compile(templateHtml);
+		})
+	},
+
+	setUseDateFormat : function(startIndex, length){
+		Handlebars.registerHelper('formattingDate', (date) =>{
+			return date.substr(startIndex, length);
+		});
+	}
+}
