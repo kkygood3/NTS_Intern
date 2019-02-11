b7f45a08ed517ccab83c50c090c05c8836fc900e
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-12 02:01:56 +0900

    기본 설정 변경

--- src/main/java/com/nts/reservation/common/config/DbConfig.java
+++ src/main/java/com/nts/reservation/common/config/DbConfig.java
@@ -2,7 +2,7 @@
  * Copyright 2019 Naver Corp. All rights Reserved.
  * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
-package com.nts.reservation.config;
+package com.nts.reservation.common.config;
 
 import javax.sql.DataSource;
 
