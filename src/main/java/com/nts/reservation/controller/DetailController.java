dc1081dc34c88b0f515aa7059f4e66c3a8889a05
siyoon <c.yun@nts-corp.com>
2019-02-11 14:53:57 +0900

    접근자 변경

--- src/main/java/com/nts/reservation/controller/DetailController.java
+++ src/main/java/com/nts/reservation/controller/DetailController.java
@@ -6,20 +6,23 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.nts.reservation.dto.DisplayInfo;
 import com.nts.reservation.dto.Product;
+import com.nts.reservation.service.CommentDisplayInfoService;
 import com.nts.reservation.service.DisplayInfoService;
 import com.nts.reservation.service.FileInfoService;
 import com.nts.reservation.service.ProductService;
+import com.nts.reservation.service.ReservationUserCommentService;
 
 @Controller
 public class DetailController {
 	@Autowired
-	FileInfoService fileInfoService;
+	private FileInfoService fileInfoService;
 	@Autowired
-	ProductService productService;
+	private ProductService productService;
 	@Autowired
-	DisplayInfoService displayInfoService;
+	private DisplayInfoService displayInfoService;
+	@Autowired
+	private ReservationUserCommentService reservationUserCommentService;
 	/**
 	 * 상세페이지 URL 맵핑 
 	 * @return 메인페이지
@@ -29,17 +32,12 @@ public class DetailController {
 		@RequestParam(name = "display_info_id", required = true) long displayInfoId,
 		ModelMap model) {
 		Product product = productService.getProduct(productId);
-		DisplayInfo displayInfo = displayInfoService.getDisplayInfo(displayInfoId);
-		
 		model.addAttribute("productId", productId);
-		model.addAttribute("displayInfoId", displayInfoId);
 		model.addAttribute("description", product.getDescription());
 		model.addAttribute("content", product.getContent());
-		model.addAttribute("placeName", displayInfo.getPlaceName());
-		model.addAttribute("placeStreet", displayInfo.getPlaceStreet());
-		model.addAttribute("placeLot", displayInfo.getPlaceLot());
-		model.addAttribute("tel", displayInfo.getTel());
 		model.addAttribute("saveFileName", fileInfoService.getSaveFileName(productId, "ma"));
+		model.addAttribute("averageScore", reservationUserCommentService.getAverageScore(productId));
+		model.addAttribute("commentCount", reservationUserCommentService.getCount(productId));
 		
 		return "detail";
 	}
