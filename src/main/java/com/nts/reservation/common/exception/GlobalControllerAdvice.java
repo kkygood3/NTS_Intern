d7ceaaf7a28a8e4dd08b00e226d1278ff4b3dbfd
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-12 02:06:11 +0900

    잘못된 오류 처리 (error -> exception)

--- src/main/java/com/nts/reservation/common/exception/GlobalControllerAdvice.java
+++ src/main/java/com/nts/reservation/common/exception/GlobalControllerAdvice.java
@@ -2,19 +2,23 @@
  * Copyright 2019 Naver Corp. All rights Reserved.
  * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
-package com.nts.reservation.exception;
+package com.nts.reservation.common.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalControllerAdvice {
 
-	@ExceptionHandler(value = IllegalAccessError.class)
-	public ModelAndView handleException500(IllegalAccessError e) {
-		ModelAndView mav = new ModelAndView("common/error");
-		mav.addObject("errorMessage", e.getMessage());
-		return mav;
+	@ExceptionHandler(value = IllegalArgumentException.class)
+	public ModelAndView handleException400(IllegalArgumentException e) {
+		e.printStackTrace();
+		
+		ModelAndView modelAndView = new ModelAndView("common/error");
+		modelAndView.addObject("errorMessage", "BAD_REQUEST!! Please try agin...");
+		modelAndView.setStatus(HttpStatus.BAD_REQUEST);
+		return modelAndView;
 	}
 }
