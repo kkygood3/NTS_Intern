42cc68a205450a5d6b85e8d040bc153715b17b2d
siyoon <c.yun@nts-corp.com>
2019-02-11 16:29:12 +0900

    함수 분리

--- src/main/webapp/javascript/detail/visual.js
+++ src/main/webapp/javascript/detail/visual.js
@@ -5,16 +5,24 @@ function sendGetEtcImageAjax() {
 function setVisualArea(etcImage) {
 	var template = document.getElementById("visual_area").innerText;
 	var sectionVisual = document.getElementsByClassName("section_visual")[0];
+	
 	template = template.replace("{image_length}", etcImage.length > 0 ? 2 : 1);
 	var bindTemplate = Handlebars.compile(template); 
-	if (etcImage.length > 0) {
-		etcImage = {"etcImage" : etcImage[0]};
-	} else {
-		etcImage = null;
-	}
-	var resultHTML = sectionVisual.innerHTML + bindTemplate(etcImage);
-	sectionVisual.innerHTML = resultHTML;
+	etcImage = preprocessEtcImage(etcImage);
 	
+	sectionVisual.innerHTML += bindTemplate(etcImage);
+	adjustUlSize(etcImage);
+}
+
+function preprocessEtcImage(etcImage) {
+	if (etcImage.length > 0) {
+		return {"etcImage" : etcImage[0]};
+	} else {
+		return null;
+	}
+}
+
+function adjustUlSize(etcImage) {
 	if (etcImage) {
 		var ul = document.getElementsByClassName("visual_img");
 		ul[0].style.width="200%";
