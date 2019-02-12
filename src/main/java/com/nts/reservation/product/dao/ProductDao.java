434993e751f67f5a70263d2de5a67a999ace7c1d
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-13 02:59:27 +0900

    product의 Images, prices 조회 method 작성

--- src/main/java/com/nts/reservation/product/dao/ProductDao.java
+++ src/main/java/com/nts/reservation/product/dao/ProductDao.java
@@ -7,9 +7,15 @@ package com.nts.reservation.product.dao;
 import java.util.List;
 
 import com.nts.reservation.product.dto.Product;
+import com.nts.reservation.product.dto.ProductImage;
+import com.nts.reservation.product.dto.ProductPrice;
 
 public interface ProductDao {
 	List<Product> selectProductsByCategory(int categoryId, int start, int limit);
 
 	int selectProductCountByCategory(int categoryId);
+
+	List<ProductImage> selectProductImages(int displayInfoId);
+
+	List<ProductPrice> selectProductPrices(int displayInfoId);
 }
--- src/main/java/com/nts/reservation/product/dao/daoImpl/ProductDaoImpl.java
+++ src/main/java/com/nts/reservation/product/dao/daoImpl/ProductDaoImpl.java
@@ -18,16 +18,20 @@ import org.springframework.stereotype.Repository;
 import com.nts.reservation.product.dao.ProductDao;
 import com.nts.reservation.product.dao.sqls.ProductDaoSqls;
 import com.nts.reservation.product.dto.Product;
+import com.nts.reservation.product.dto.ProductImage;
+import com.nts.reservation.product.dto.ProductPrice;
 
 @Repository
-public class ProductDaoImpl implements ProductDao{
+public class ProductDaoImpl implements ProductDao {
 	private NamedParameterJdbcTemplate jdbc;
 
 	public ProductDaoImpl(DataSource dataSource) {
 		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
 	}
 
-	private RowMapper<Product> rowMapper = BeanPropertyRowMapper.newInstance(Product.class);
+	private RowMapper<Product> productRowMapper = BeanPropertyRowMapper.newInstance(Product.class);
+	private RowMapper<ProductImage> productImageRowMapper = BeanPropertyRowMapper.newInstance(ProductImage.class);
+	private RowMapper<ProductPrice> productPriceRowMapper = BeanPropertyRowMapper.newInstance(ProductPrice.class);
 
 	private static final int ALL_CATEGORIES = 0;
 
@@ -39,9 +43,9 @@ public class ProductDaoImpl implements ProductDao{
 		params.put("limit", limit);
 
 		if (categoryId == ALL_CATEGORIES) {
-			return jdbc.query(ProductDaoSqls.GET_ALL_PRODUCTS, params, rowMapper);
+			return jdbc.query(ProductDaoSqls.GET_ALL_PRODUCTS, params, productRowMapper);
 		} else {
-			return jdbc.query(ProductDaoSqls.GET_PRODUCTS_BY_CATEGORY, params, rowMapper);
+			return jdbc.query(ProductDaoSqls.GET_PRODUCTS_BY_CATEGORY, params, productRowMapper);
 		}
 	}
 
@@ -55,5 +59,19 @@ public class ProductDaoImpl implements ProductDao{
 		} else {
 			return jdbc.queryForObject(ProductDaoSqls.GET_PRODUCTS_COUNT_BY_CATEGORY, params, Integer.class);
 		}
+	}
+
+	@Override
+	public List<ProductImage> selectProductImages(int displayInfoId) {
+		Map<String, Integer> param = new HashMap<>();
+		param.put("displayInfoId", displayInfoId);
+		return jdbc.query(ProductDaoSqls.GET_PRODUCT_IMAGES, param, productImageRowMapper);
+	}
+
+	@Override
+	public List<ProductPrice> selectProductPrices(int displayInfoId) {
+		Map<String, Integer> param = new HashMap<>();
+		param.put("displayInfoId", displayInfoId);
+		return jdbc.query(ProductDaoSqls.GET_PRODUCT_PRICES, param, productPriceRowMapper);
 	}
 }
