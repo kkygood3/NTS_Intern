5533a12bccfa3f33223532f99f5f1f0773bb0e97
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-12 18:55:27 +0900

    DisplayDao class 생성

---
+++ src/main/java/com/nts/reservation/display/dao/DisplayDao.java
@@ -0,0 +1,9 @@
+/**
+ * Copyright 2019 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.nts.reservation.display.dao;
+
+public interface DisplayDao {
+
+}
