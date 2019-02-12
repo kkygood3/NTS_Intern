4276e49fbb00af4a7ccab389ef4ae55c25b34256
siyoon <c.yun@nts-corp.com>
2019-02-12 18:53:55 +0900

    어노테이션 추가

--- src/main/java/com/nts/reservation/dao/DatailPageInfoDao.java
+++ src/main/java/com/nts/reservation/dao/DatailPageInfoDao.java
@@ -10,9 +10,11 @@ import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 import com.nts.reservation.dto.DatailPageInfo;
 
+@Repository
 public class DatailPageInfoDao extends BaseDao {
 	RowMapper<DatailPageInfo> rowMapper = BeanPropertyRowMapper.newInstance(DatailPageInfo.class);
 	DatailPageInfoDao(DataSource dataSource) {
