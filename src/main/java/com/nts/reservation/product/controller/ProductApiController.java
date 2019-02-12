3c181ddf33c9d573ae70f2186484ede910602d07
박두익 <duik.park@nts-corp.com>
2019-02-12 14:08:37 +0900

    ProductApiController가 객체를 반환하도록 수정

--- src/main/java/com/nts/reservation/product/controller/ProductApiController.java
+++ src/main/java/com/nts/reservation/product/controller/ProductApiController.java
@@ -5,16 +5,13 @@
 package com.nts.reservation.product.controller;
 
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nts.reservation.product.dto.Product;
+import com.nts.reservation.product.dto.ProductList;
 import com.nts.reservation.product.service.ProductService;
 
 /**
@@ -25,29 +22,29 @@ public class ProductApiController {
 	@Autowired
 	private ProductService productService;
 
+	private ProductList productList = new ProductList();
+
 	@GetMapping("/api/products")
-	public Map<String, Object> getProductList(
+	public ProductList getProductList(
 		@RequestParam(name = "categoryId", required = false, defaultValue = "0") int categoryId,
 		@RequestParam(name = "start", required = false, defaultValue = "0") int start,
 		@RequestParam(name = "limit", required = false, defaultValue = "4") int limit) {
 
 		if (isInvalidParameter(categoryId, start)) {
 			System.out.println("올바르지 않은 categoryId 또는 start");
-			return Collections.emptyMap();
+
+			return getEmptyProductList();
 		}
 
 		int productCount = productService.getProductsCountByCategoryId(categoryId);
 		if (productCount == 0) {
-			return Collections.emptyMap();
+			return getEmptyProductList();
 		}
 
-		List<Product> productList = productService.getProducts(categoryId, start, limit);
+		productList.setProductCount(productService.getProductsCountByCategoryId(categoryId));
+		productList.setProductList(productService.getProducts(categoryId, start, limit));
 
-		Map<String, Object> map = new HashMap<>();
-		map.put("productList", productList);
-		map.put("productCount", productCount);
-
-		return map;
+		return productList;
 	}
 
 	private boolean isValidParameter(int categoryId, int limit) {
@@ -59,5 +56,12 @@ public class ProductApiController {
 
 	private boolean isInvalidParameter(int categoryId, int limit) {
 		return !isValidParameter(categoryId, limit);
+	}
+
+	private ProductList getEmptyProductList() {
+		productList.setProductCount(0);
+		productList.setProductList(Collections.emptyList());
+
+		return productList;
 	}
 }
---
+++ src/main/java/com/nts/reservation/product/dto/ProductList.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2019 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.nts.reservation.product.dto;
+
+import java.util.List;
+
+/**
+ * @Author Duik Park, duik.park@nts-corp.com
+ */
+public class ProductList {
+	private int productCount;
+	private List<Product> productList;
+
+	public int getProductCount() {
+		return productCount;
+	}
+
+	public void setProductCount(int productCount) {
+		this.productCount = productCount;
+	}
+
+	public List<Product> getProductList() {
+		return productList;
+	}
+
+	public void setProductList(List<Product> productList) {
+		this.productList = productList;
+	}
+}
