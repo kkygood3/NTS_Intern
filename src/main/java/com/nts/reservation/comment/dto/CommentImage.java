1f58707b748b11b1cea1c3c05374fbd6a63b3148
박두익 <duik.park@nts-corp.com>
2019-02-13 10:35:45 +0900

    Comment DTO 작성

--- src/main/java/com/nts/reservation/comment/dto/CommentImage.java
+++ src/main/java/com/nts/reservation/comment/dto/CommentImage.java
@@ -8,5 +8,14 @@ package com.nts.reservation.comment.dto;
  * @Author Duik Park, duik.park@nts-corp.com
  */
 public class CommentImage {
-
+	private String contentType;
+	private String createDate;
+	private boolean deleteFlag;
+	private int fileId;
+	private String fileName;
+	private int imageId;
+	private String modifyDate;
+	private int reservationInfoId;
+	private int reservationUserCommentId;
+	private String saveFileName;
 }
