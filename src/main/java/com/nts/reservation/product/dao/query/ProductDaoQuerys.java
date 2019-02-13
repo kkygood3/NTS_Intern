4540e10fa6e4f72d9d2d08a47c1a57bc24df219a
박두익 <duik.park@nts-corp.com>
2019-02-13 16:40:07 +0900

    쿼리 수정

--- src/main/java/com/nts/reservation/product/dao/query/ProductDaoQuerys.java
+++ src/main/java/com/nts/reservation/product/dao/query/ProductDaoQuerys.java
@@ -15,12 +15,12 @@ public class ProductDaoQuerys {
 		+ " LIMIT :start, :limit";
 
 	public static final String SELECT_PRODUCT_BY_CATEGORY = "SELECT display_info.id AS display_info_id, display_info.place_name, product.content AS product_content, product.description AS product_description, product.id AS product_id, file_info.save_file_name AS product_image_url "
-		+ "FROM product "
-		+ "INNER JOIN product_image ON product.id = product_image.product_id "
-		+ "INNER JOIN display_info ON product.id = display_info.product_id "
-		+ "INNER JOIN file_info ON product_image.file_id = file_info.id and product_image.type='th' "
-		+ "WHERE category_id = :categoryId "
-		+ "LIMIT :start, :limit;";
+		+ " FROM product "
+		+ " INNER JOIN product_image ON product.id = product_image.product_id "
+		+ " INNER JOIN display_info ON product.id = display_info.product_id "
+		+ " INNER JOIN file_info ON product_image.file_id = file_info.id and product_image.type='th' "
+		+ " WHERE category_id = :categoryId "
+		+ " LIMIT :start, :limit;";
 
 	public static final String SELECT_PRODUCT_COUNT = "SELECT COUNT(display_info.id) FROM product"
 		+ " INNER JOIN product_image ON product.id = product_image.product_id and product_image.type = 'th'"
