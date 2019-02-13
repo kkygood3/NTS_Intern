21b3e02a4d50df6ac613dc9ad04e2a82b3bfe815
Yook Sung Ryul <yook0328@gmail.com>
2019-02-13 17:17:30 +0900

    ReservationDao 및 SQL 추가

---
+++ src/main/java/com/nts/reservation/dao/ReservationDao.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright 2019 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.nts.reservation.dao;
+
+import static com.nts.reservation.dao.sql.ReservationDaoSqls.INSERT_RESERVATION_INFO;
+import static com.nts.reservation.dao.sql.ReservationDaoSqls.INSERT_RESERVATION_INFO_PRICE;
+import static com.nts.reservation.dao.sql.ReservationDaoSqls.SELECT_COUNT_RESERVATION_BY_EMAIL;
+import static com.nts.reservation.dao.sql.ReservationDaoSqls.SELECT_RESERVATION_INFO_BY_EMAIL;
+import static com.nts.reservation.dao.sql.ReservationDaoSqls.UPDATE_CANCEL_RESERVATION_BY_RESERVATION_ID;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import com.nts.reservation.dto.ReservationInfoDto;
+import com.nts.reservation.dto.request.ReservationPriceRequestDto;
+import com.nts.reservation.dto.request.ReservationRequestDto;
+
+/**
+ * @author 육성렬
+ */
+@Repository
+public class ReservationDao {
+	private NamedParameterJdbcTemplate jdbc;
+	private RowMapper<ReservationInfoDto> reservationRowMapper = BeanPropertyRowMapper
+		.newInstance(ReservationInfoDto.class);
+
+	public ReservationDao(DataSource dataSource) {
+		jdbc = new NamedParameterJdbcTemplate(dataSource);
+	}
+
+	public Long insertReservation(ReservationRequestDto reservation) {
+		SqlParameterSource params = new BeanPropertySqlParameterSource(reservation);
+		KeyHolder holder = new GeneratedKeyHolder();
+		jdbc.update(INSERT_RESERVATION_INFO, params, holder);
+		return holder.getKey().longValue();
+	}
+
+	public int insertReservationPrice(long reservationInfoId, long productPriceId, int count) {
+		Map<String, Object> map = new HashMap();
+		map.put("reservationInfoId", reservationInfoId);
+		map.put("productPriceId", productPriceId);
+		map.put("count", count);
+		return jdbc.update(INSERT_RESERVATION_INFO_PRICE, map);
+	}
+
+	public int[] insertReservationPrices(List<ReservationPriceRequestDto> priceList) {
+		SqlParameterSource[] batchs = SqlParameterSourceUtils.createBatch(priceList.toArray());
+
+		return jdbc.batchUpdate(INSERT_RESERVATION_INFO_PRICE, batchs);
+	}
+
+	public List<ReservationInfoDto> selectReservationsByEmail(String email) {
+		Map<String, Object> map = Collections.singletonMap("email", email);
+		return jdbc.query(SELECT_RESERVATION_INFO_BY_EMAIL, map, reservationRowMapper);
+	}
+
+	public int countReservationsByEmail(String email) {
+		Map<String, Object> map = Collections.singletonMap("email", email);
+		return jdbc.queryForObject(SELECT_COUNT_RESERVATION_BY_EMAIL, map, Integer.class);
+	}
+
+	public Integer updateCancelReservation(Long reservationInfoId) {
+		Map<String, Object> map = Collections.singletonMap("id", reservationInfoId);
+		int resultRows = jdbc.update(UPDATE_CANCEL_RESERVATION_BY_RESERVATION_ID, map);
+		return resultRows;
+	}
+}
---
+++ src/main/java/com/nts/reservation/dao/sql/ReservationDaoSqls.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2019 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.nts.reservation.dao.sql;
+
+/**
+ * @author 육성렬
+ */
+public class ReservationDaoSqls {
+	public static final String INSERT_RESERVATION_INFO = "INSERT INTO reservation_info"
+		+ " (product_id"
+		+ ", display_info_id"
+		+ ", reservation_name"
+		+ ", reservation_tel"
+		+ ", reservation_email"
+		+ ", reservation_date"
+		+ ", create_date"
+		+ ", modify_date)"
+		+ " VALUES (:productId"
+		+ ", :displayInfoId"
+		+ ", :reservationName"
+		+ ", :reservationTel"
+		+ ", :reservationEmail"
+		+ ", NOW()"
+		+ ", NOW()"
+		+ ", NOW())";
+	public static final String INSERT_RESERVATION_INFO_PRICE = "INSERT INTO reservation_info_price"
+		+ " ( reservation_info_id"
+		+ ", product_price_id"
+		+ ", count )"
+		+ " VALUES ("
+		+ " :reservationInfoId"
+		+ ", :productPriceId"
+		+ ", :count)";
+	public static final String SELECT_RESERVATION_INFO_BY_EMAIL = "SELECT SUM(pd_price.price * rv_price.count) AS total_price,"
+		+ "reservation.cancel_flag AS cancel_yn"
+		+ ", reservation.create_date"
+		+ ", reservation.display_info_id"
+		+ ", reservation.modify_date"
+		+ ", reservation.product_id"
+		+ ", reservation.reservation_name"
+		+ ", reservation.reservation_email"
+		+ ", reservation.reservation_date"
+		+ ", reservation.reservation_tel"
+		+ ", reservation.id AS reservation_info_id"
+		+ " FROM reservation_info AS reservation"
+		+ " INNER JOIN reservation_info_price AS rv_price ON reservation.id = rv_price.reservation_info_id"
+		+ " INNER JOIN display_info AS display ON reservation.display_info_id = display.id"
+		+ " INNER JOIN product ON product.id = reservation.product_id"
+		+ " INNER JOIN category ON product.category_id = category.id"
+		+ " INNER JOIN product_price AS pd_price ON pd_price.id = rv_price.product_price_id"
+		+ " WHERE reservation.reservation_email = :email"
+		+ " GROUP BY rv_price.reservation_info_id";
+
+	public static final String SELECT_COUNT_RESERVATION_BY_EMAIL = "SELECT COUNT(reservation.id)"
+		+ " FROM reservation_info AS reservation"
+		+ " WHERE reservation.reservation_email = :email";
+
+	public static final String UPDATE_CANCEL_RESERVATION_BY_RESERVATION_ID = "UPDATE reservation_info"
+		+ " SET cancel_flag = true"
+		+ " WHERE id = :id";
+}
