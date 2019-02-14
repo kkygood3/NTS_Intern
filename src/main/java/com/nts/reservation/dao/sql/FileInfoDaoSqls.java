b712f923e69a22d225086780649484acdb4a0fca
siyoon <c.yun@nts-corp.com>
2019-02-14 14:06:48 +0900

    들여쓰기 제거

--- src/main/java/com/nts/reservation/dao/sql/FileInfoDaoSqls.java
+++ src/main/java/com/nts/reservation/dao/sql/FileInfoDaoSqls.java
@@ -4,8 +4,8 @@ public class FileInfoDaoSqls {
 	public static final String SELECT_BY_PRODUCT_ID_AND_TYPE
 	= "SELECT file_info.save_file_name "
 	+ "FROM product "
-	+ "	INNER JOIN product_image ON product.id = product_image.product_id "
-	+ "	INNER JOIN file_info ON product_image.file_id = file_info.id "
+	+ "INNER JOIN product_image ON product.id = product_image.product_id "
+	+ "INNER JOIN file_info ON product_image.file_id = file_info.id "
 	+ "WHERE product_image.type = :type AND product.id = :productId";
 	
 }
