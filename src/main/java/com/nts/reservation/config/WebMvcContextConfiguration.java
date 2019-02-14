ad9441494a23ecb8f0ad25a66bb4232b380cddd1
siyoon <c.yun@nts-corp.com>
2019-02-14 10:38:33 +0900

    불필요한 코드 제거

--- src/main/java/com/nts/reservation/config/WebMvcContextConfiguration.java
+++ src/main/java/com/nts/reservation/config/WebMvcContextConfiguration.java
@@ -6,7 +6,6 @@ import static com.nts.reservation.config.ProjectInfo.CACHE_PERIOD;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
