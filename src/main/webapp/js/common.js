0b8761ace62eebaf7c53ed64fc5201a99c645dbd
박우성 <woosung.park@nts-corp.com>
2019-02-14 14:45:54 +0900

    주석 작성

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -1,8 +1,7 @@
 /**
- * 메인 페이지에서 호출할 /api의 Ajax Reqeust를 전담.
- * 
- * @param callback 페이지 로드후에 실행할 callback function
- * @param url
+ * 반복적으로 호출하는 XMLHttpRequest를 전담하는 함수
+ * @param 페이지 로드후에 실행할 callback function. 매개변수로 response를 가져아 함 
+ * @param api URL
  */
 function requestAjax(callback, url) {
 	var ajaxReq = new XMLHttpRequest();
@@ -10,12 +9,16 @@ function requestAjax(callback, url) {
 	ajaxReq.addEventListener('load', function(evt) {
 		this.callback(evt.target.response);
 	});
-
 	ajaxReq.open('GET', url);
 	ajaxReq.responseType = 'json';
 	ajaxReq.send()
 }
 
+/**
+ * URL parameter의 value를 얻어내기 위한 함수
+ * @param parameter name
+ * @return parameter value
+ */
 function getUrlParameter(name) {
 	var params = location.href.split('?')[1].split('&');
 	for (var i = 0; i < params.length; i++) {
