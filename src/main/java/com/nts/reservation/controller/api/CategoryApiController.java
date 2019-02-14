4591b50c73debc0343a44771163c3f0b6b3bd202
박우성 <woosung.park@nts-corp.com>
2019-02-14 12:14:57 +0900

    .java 라이센스 주석 추가

--- src/main/java/com/nts/reservation/controller/api/CategoryApiController.java
+++ src/main/java/com/nts/reservation/controller/api/CategoryApiController.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright 2019 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
 package com.nts.reservation.controller.api;
 
 import java.util.HashMap;
--- src/main/java/com/nts/reservation/dao/detail/DetailResponseDao.java
+++ src/main/java/com/nts/reservation/dao/detail/DetailResponseDao.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright 2019 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
 package com.nts.reservation.dao.detail;
 
 import java.util.HashMap;
