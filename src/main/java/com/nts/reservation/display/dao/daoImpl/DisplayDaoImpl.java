75ce3a18845eeaed61585884ce4fbddcebc6e3e4
yongjoon.Park <yongjoon.Park@nts-corp.com>
2019-02-14 18:34:43 +0900

    DataAccessUtils 사용하여 null 반환, SQL query 수정

--- src/main/java/com/nts/reservation/display/dao/daoImpl/DisplayDaoImpl.java
+++ src/main/java/com/nts/reservation/display/dao/daoImpl/DisplayDaoImpl.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -19,9 +20,10 @@ import com.nts.reservation.display.dto.DisplayInfoImage;
 public class DisplayDaoImpl implements DisplayDao {
 
 	private NamedParameterJdbcTemplate jdbc;
-	
+
 	private RowMapper<DisplayInfo> displayInfoRowMapper = BeanPropertyRowMapper.newInstance(DisplayInfo.class);
-	private RowMapper<DisplayInfoImage> displayInfoImageRowMapper = BeanPropertyRowMapper.newInstance(DisplayInfoImage.class);
+	private RowMapper<DisplayInfoImage> displayInfoImageRowMapper = BeanPropertyRowMapper
+		.newInstance(DisplayInfoImage.class);
 
 	public DisplayDaoImpl(DataSource dataSource) {
 		jdbc = new NamedParameterJdbcTemplate(dataSource);
@@ -31,13 +33,14 @@ public class DisplayDaoImpl implements DisplayDao {
 	public DisplayInfo selectDisplayInfo(int displayInfoId) {
 		Map<String, Integer> param = new HashMap<>();
 		param.put("displayInfoId", displayInfoId);
-		return jdbc.queryForObject(DisplayDaoSqls.GET_DISPLAY_BY_ID, param, displayInfoRowMapper);
+
+		return DataAccessUtils.singleResult(jdbc.query(DisplayDaoSqls.GET_DISPLAY_BY_ID, param, displayInfoRowMapper));
 	}
 
 	@Override
 	public DisplayInfoImage selectDisplayInfoImage(int displayInfoId) {
 		Map<String, Integer> param = new HashMap<>();
 		param.put("displayInfoId", displayInfoId);
-		return jdbc.queryForObject(DisplayDaoSqls.GET_DISPLAY_BY_ID, param, displayInfoImageRowMapper);
+		return jdbc.queryForObject(DisplayDaoSqls.GET_DISPLAY_IMAGE_BY_ID, param, displayInfoImageRowMapper);
 	}
 }
