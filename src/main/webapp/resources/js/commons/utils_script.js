a56864726b8f4c64bc70a900ff63dd13e0ae4ee0
이재원 <lee.jaewon@nts-corp.com>
2019-02-14 11:17:50 +0900

    fixed according to reviews

--- src/main/webapp/resources/js/commons/utils_script.js
+++ src/main/webapp/resources/js/commons/utils_script.js
@@ -26,12 +26,14 @@ SlidingAnimation.prototype.init = function(optionals) {
 	nextSlideCount = 1;
 	isAnimating = false;
 	isAutoStart = false;
-	debugger;
-	maxHeight = optionals["maxHeight"] ? params.maxHeight : slideWrapper.clientHeight;
-	minHeight = optionals["minHeight"] ? params.minHeight : 100;
-	animationSpeed = optionals["animationSpeed"] ? params.animationSpeed : 10;
-	animationStopDuration = optionals["animationStopDuration"] ? params.animationStopDuration : 1000;
-		
+	
+	if(optionals){
+		maxHeight = optionals.maxHeight ? params.maxHeight : slideWrapper.clientHeight;
+		minHeight = optionals.minHeight ? params.minHeight : 100;
+		animationSpeed = optionals.animationSpeed ? params.animationSpeed : 10;
+		animationStopDuration = optionals.animationStopDuration ? params.animationStopDuration : 1000;
+	}
+	
 	imageList.forEach((item) => {
 		if(item == imageList[0]) {
 			imageList[0].style.left = 0 + "px";
