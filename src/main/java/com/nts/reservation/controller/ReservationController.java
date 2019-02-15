57da87b79b85c78ad579b05b6f19c058bd0f5142
siyoon <c.yun@nts-corp.com>
2019-02-15 10:40:58 +0900

    path variable 변경

--- src/main/java/com/nts/reservation/controller/ReservationController.java
+++ src/main/java/com/nts/reservation/controller/ReservationController.java
@@ -1,10 +1,14 @@
 package com.nts.reservation.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.nts.reservation.dto.ReservationPageInfo;
+import com.nts.reservation.service.ReservationService;
 
 /**
  * 예약페이지 url맵핑해주는 클래스
@@ -13,11 +17,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping(path="reservation")
-public class ReserveController {
-	@GetMapping(path = "/{productId}")
-	public String reservation(@PathVariable(name="productId", required= true) long productId,
+public class ReservationController {
+	@Autowired
+	private ReservationService reservationService;
+	
+	@GetMapping(path = "/{displayInfoId}")
+	public String reservation(@PathVariable(name="displayInfoId", required= true) long displayInfoId,
 		ModelMap model) {
-		
+		ReservationPageInfo reservationPageInfo = reservationService.getReservationPageInfoByDisplayInfoId(displayInfoId);
+		model.addAttribute("reservationPageInfo", reservationPageInfo);
 		return "reservation";
 	}
 	
