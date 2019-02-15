01908e378cc4ab890dcab004dc3c0e47245801ec
박우성 <woosung.park@nts-corp.com>
2019-02-15 11:58:35 +0900

    패키지명 변경

--- src/main/java/com/nts/reservation/controller/error/ExceptionAdvice.java
+++ src/main/java/com/nts/reservation/controller/error/ExceptionAdvice.java
@@ -2,7 +2,7 @@
  * Copyright 2019 Naver Corp. All rights Reserved.
  * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
-package com.nts.reservation.controller;
+package com.nts.reservation.controller.error;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionAdvice {
 	/**
-	 * controller에서 받는 int형 param에 형 변환 할 수 없는 값이 들어올 때 처리하는 역할. 
+	 * controller에서 받는 int형 param에 형 변환 할 수 없는 값이 들어올 때 처리. error 페이지를 출력 
 	 */
 	@ExceptionHandler(NumberFormatException.class)
 	public String NumberFormatException(HttpServletRequest req, Exception ex) {
