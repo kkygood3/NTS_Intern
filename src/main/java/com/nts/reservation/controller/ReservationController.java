a9fea4071cf202583cf4ee8bf45a57e91acdfce0
siyoon <c.yun@nts-corp.com>
2019-02-15 14:51:46 +0900

    불필요한 코드 삭제

--- src/main/java/com/nts/reservation/controller/ReservationController.java
+++ src/main/java/com/nts/reservation/controller/ReservationController.java
@@ -1,14 +1,8 @@
 package com.nts.reservation.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.nts.reservation.dto.ReservationPageInfo;
-import com.nts.reservation.service.ReservationService;
 
 /**
  * 예약페이지 url맵핑해주는 클래스
@@ -18,8 +12,6 @@ import com.nts.reservation.service.ReservationService;
 @Controller
 @RequestMapping(path="reservation")
 public class ReservationController {
-
-	
 	@GetMapping
 	public String myReservation() {
 		return "myreservation";
