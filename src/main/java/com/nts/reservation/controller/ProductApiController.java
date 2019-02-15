b9bf580f5519f2a120f9f7e68e61051ef1aa582e
siyoon <c.yun@nts-corp.com>
2019-02-15 14:57:54 +0900

    경로같은 컨트롤러 병합

--- src/main/java/com/nts/reservation/controller/ProductApiController.java
+++ src/main/java/com/nts/reservation/controller/ProductApiController.java
@@ -11,12 +11,14 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nts.reservation.dto.ProductThumbnail;
+import com.nts.reservation.service.FileInfoService;
 import com.nts.reservation.service.ProductService;
 import com.nts.reservation.service.ProductThumbnailService;
 
@@ -59,5 +61,23 @@ public class ProductApiController {
 		map.put("productCount", productCount);
 		map.put("thumbnailInfoList", thumbnailInfoList);
 		return map;
+	}
+	
+
+	@Autowired
+	private FileInfoService fileInfoService;
+
+	/**
+	 * 상품명이랑 타입에 맞는 이미지 가져온다
+	 * @param productId 조회할 상품 id
+	 * @param type 조회할 타입 ma(main), et(etc), th(thumbnail)
+	 * @return 해당 파일 이름들
+	 */
+	@GetMapping("/{productId}/file_info/{type}")
+	@ResponseStatus(HttpStatus.OK)
+	public List<String> getProductCountAndThumbnailInfos(
+		@PathVariable(name = "productId", required = true) int productId,
+		@PathVariable(name = "type", required = true) String type) {
+		return fileInfoService.getSaveFileName(productId, type);
 	}
 }
