aeb90039dbe0038eddb7a9287abeb981c8862247
박우성 <woosung.park@nts-corp.com>
2019-02-15 16:04:44 +0900

    review 페이지 SELECT SQL 변경

--- src/main/java/com/nts/reservation/dao/sql/ReviewResponseSqls.java
+++ src/main/java/com/nts/reservation/dao/sql/ReviewResponseSqls.java
@@ -5,21 +5,12 @@
 package com.nts.reservation.dao.sql;
 
 public class ReviewResponseSqls {
-	public static String SELECT_REVIEW = 
-		"SELECT display_info.id AS display_info_id,reservation_user_comment.comment,file_info.save_file_name AS comment_image, reservation_info.reservation_name, DATE_FORMAT(reservation_info.reservation_date,'%Y.%c.%d') AS reservation_date,reservation_user_comment.score,product.description AS product_description, average_score_subquery.average_score, average_score_subquery.comment_count" + 
-		" FROM display_info " + 
-		" LEFT OUTER JOIN reservation_info ON display_info.id = reservation_info.display_info_id " + 
-		" LEFT OUTER JOIN reservation_user_comment ON reservation_info.id = reservation_user_comment.reservation_info_id " + 
-		" LEFT JOIN reservation_user_comment_image ON reservation_user_comment.id = reservation_user_comment_image.reservation_user_comment_id " + 
-		" LEFT JOIN product ON display_info.product_id = product.id " + 
-		" LEFT JOIN file_info ON reservation_user_comment_image.file_id = file_info.id" + 
-		
-		" INNER JOIN (SELECT display_info.id, ROUND(AVG(reservation_user_comment.score),1)  AS average_score, COUNT(reservation_user_comment.id) AS comment_count FROM reservation_user_comment " + 
-		" INNER JOIN product ON reservation_user_comment.product_id = product.id " + 
-		" INNER JOIN display_info ON product.id = display_info.id " + 
-		" WHERE display_info.id = :displayInfoId) AS average_score_subquery ON display_info.id = average_score_subquery.id" +
-		
-		" WHERE display_info.id = :displayInfoId " + 
-		" ORDER BY reservation_user_comment.id DESC " + 
-		" LIMIT :start, :pagingLimit";
+	public static final String SELECT_REVIEW_DISPLAY_INFO = 
+		"SELECT display_info.id AS display_info_id, product.description AS product_description, IFNULL(ROUND(AVG(reservation_user_comment.score),1),0) AS average_score, COUNT(reservation_user_comment.id) as comment_count" + 
+		" FROM display_info" + 
+		" INNER JOIN product ON display_info.product_id = product.id" + 
+		" INNER JOIN reservation_user_comment ON product.id = reservation_user_comment.product_id" + 
+		" WHERE display_info.id = :displayInfoId";
+	
+	public static final String SELECT_REVIEW_COMMENT = com.nts.reservation.dao.sql.common.CommonSqls.SELECT_COMMENT;
 }
