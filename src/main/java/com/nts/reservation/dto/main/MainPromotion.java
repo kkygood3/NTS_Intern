0298d8d94132fa808d92b16b9e296575c224ed38
박우성 <woosung.park@nts-corp.com>
2019-02-15 11:02:50 +0900

    불필요한 멤버 삭제

--- src/main/java/com/nts/reservation/dto/main/MainPromotion.java
+++ src/main/java/com/nts/reservation/dto/main/MainPromotion.java
@@ -9,35 +9,7 @@ package com.nts.reservation.dto.main;
  */
 public class MainPromotion {
 	private Integer displayInfoId;
-	private String placeName;
-	private String productContent;
-	private String productDescription;
-	private Integer productId;
 	private String productImageUrl;
-
-	public String getPlaceName() {
-		return placeName;
-	}
-
-	public void setPlaceName(String placeName) {
-		this.placeName = placeName;
-	}
-
-	public String getProductContent() {
-		return productContent;
-	}
-
-	public void setProductContent(String productContent) {
-		this.productContent = productContent;
-	}
-
-	public String getProductDescription() {
-		return productDescription;
-	}
-
-	public void setProductDescription(String productDescription) {
-		this.productDescription = productDescription;
-	}
 
 	public Integer getDisplayInfoId() {
 		return displayInfoId;
@@ -45,14 +17,6 @@ public class MainPromotion {
 
 	public void setDisplayInfoId(Integer displayInfoId) {
 		this.displayInfoId = displayInfoId;
-	}
-
-	public Integer getProductId() {
-		return productId;
-	}
-
-	public void setProductId(Integer productId) {
-		this.productId = productId;
 	}
 
 	public String getProductImageUrl() {
