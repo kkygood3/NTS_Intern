6f355d3fbd6ed80938744a967b6d62c5699e5a60
박우성 <woosung.park@nts-corp.com>
2019-02-16 11:08:55 +0900

    오타 수정

--- src/main/java/com/nts/reservation/service/review/ReviewResponseService.java
+++ src/main/java/com/nts/reservation/service/review/ReviewResponseService.java
@@ -11,7 +11,7 @@ public interface ReviewResponseService {
 	 * api/products/{diplayInfoId}/review response에 사용
 	 * @param displayInfoId - 조회할 displayInfo table의 ID
 	 * @param pagingLimit - 한 페이지에 출력할 item 개수
-	 * @return displayInfoId에 ReviewResponse
+	 * @return ReviewResponse
 	 */
 	ReviewResponse getReviewResponse(int displayInfoId, int start, int pagingLimit);
 }
