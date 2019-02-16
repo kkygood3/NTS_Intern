e28ffe8f861fad175d04b99fe3c6012a831bed16
박우성 <woosung.park@nts-corp.com>
2019-02-16 13:32:55 +0900

    사용하지 않는 파일 삭제

--- src/main/java/com/nts/reservation/property/ReviewProperties.java
+++
@@ -1,8 +0,0 @@
-/**
- * Copyright 2019 Naver Corp. All rights Reserved.
- * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
-package com.nts.reservation.property;
-
-public class ReviewProperties {
-}
