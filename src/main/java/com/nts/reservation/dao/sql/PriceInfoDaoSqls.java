3bd57ba4301d5a4fe381ecc2ec0aa4ac31f0314a
USER <USER@AL01288279>
2019-02-17 14:16:59 +0900

    select product price id

--- src/main/java/com/nts/reservation/dao/sql/PriceInfoDaoSqls.java
+++ src/main/java/com/nts/reservation/dao/sql/PriceInfoDaoSqls.java
@@ -2,7 +2,7 @@ package com.nts.reservation.dao.sql;
 
 public class PriceInfoDaoSqls {
 	public static final String SELECT_PRICE_INFO_BY_PRODUCT_ID
-	= "SELECT pp.price_type_name, pp.price, pp.discount_rate " 
+	= "SELECT pp.id AS product_price_id, pp.price_type_name, pp.price, pp.discount_rate " 
 	+ "FROM product AS p "
 	+ "JOIN product_price AS pp ON p.id = :productId AND p.id = pp.product_id";
 }
