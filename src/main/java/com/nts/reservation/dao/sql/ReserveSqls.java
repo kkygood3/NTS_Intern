d8f3793ff3b7717be06b889413f8efc2685c7df8
박우성 <woosung.park@nts-corp.com>
2019-02-18 16:37:24 +0900

    reserve INSERT SQL 작성

--- src/main/java/com/nts/reservation/dao/sql/ReserveSqls.java
+++ src/main/java/com/nts/reservation/dao/sql/ReserveSqls.java
@@ -27,4 +27,49 @@ public class ReserveSqls {
 		" FROM display_info" + 
 		" INNER JOIN product_price ON display_info.product_id = product_price.product_id" + 
 		" WHERE display_info.id = :displayInfoId";
+	
+	
+	public static final String INSERT_RESERVE =
+		"INSERT" + 
+		" INTO reservation_info(" + 
+		"  product_id," + 
+		"  display_info_id," + 
+		"  reservation_name," + 
+		"  reservation_tel," + 
+		"  reservation_email," + 
+		"  reservation_date," + 
+		"  cancel_flag," + 
+		"  create_date," + 
+		"  modify_date" + 
+		" )" + 
+		" SELECT" + 
+		"  product.id," + 
+		"  display_info.id," + 
+		"  :name," + 
+		"  :telephone," + 
+		"  :email," + 
+		"  NOW()," + 
+		"  0," + 
+		"  NOW()," + 
+		"  NOW()" + 
+		"  FROM display_info" + 
+		"  INNER JOIN product ON display_info.product_id = product.id" + 
+		"  WHERE display_info.id = :displayInfoId" + 
+		"  GROUP BY product.id";
+	
+	public static final String INSERT_RESERVE_PRICE =
+		"INSERT" + 
+		" INTO reservation_info_price(" + 
+		"  reservation_info_id," + 
+		"  product_price_id," + 
+		"  count" + 
+		" )" + 
+		" SELECT" + 
+		"  :reservationInfoId," + 
+		"  product_price.id," + 
+		"  :count" + 
+		"  FROM display_info" + 
+		"  INNER JOIN product_price ON display_info.product_id = product_price.product_id" + 
+		"  WHERE product_price.price_type_name = :type AND display_info.id = :displayInfoId";
+		
 }
