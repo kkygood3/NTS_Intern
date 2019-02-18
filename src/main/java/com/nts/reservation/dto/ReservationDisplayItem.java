6a82f10c6e56ad0aded10b2f6450ce1e612fea05
siyoon <c.yun@nts-corp.com>
2019-02-18 14:51:07 +0900

    DTO 생성

---
+++ src/main/java/com/nts/reservation/dto/ReservationDisplayItem.java
@@ -0,0 +1,57 @@
+package com.nts.reservation.dto;
+
+import java.util.Date;
+
+public class ReservationDisplayItem {
+	private long reservationId;
+	private String description;
+	private Date reservationDate;
+	private String placeName;
+	private long price;
+
+	public long getReservationId() {
+		return reservationId;
+	}
+
+	public void setReservationId(long reservationId) {
+		this.reservationId = reservationId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getReservationDate() {
+		return reservationDate;
+	}
+
+	public void setReservationDate(Date reservationDate) {
+		this.reservationDate = reservationDate;
+	}
+
+	public String getPlaceName() {
+		return placeName;
+	}
+
+	public void setPlaceName(String placeName) {
+		this.placeName = placeName;
+	}
+
+	public long getPrice() {
+		return price;
+	}
+
+	public void setPrice(long price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "ReservationDisplayItem [reservationId=" + reservationId + ", description=" + description
+			+ ", reservationDate=" + reservationDate + ", placeName=" + placeName + ", price=" + price + "]";
+	}
+}
