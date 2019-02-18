03cdb9daa95b998d58227a9d66e4c2ca6b084ee3
박우성 <woosung.park@nts-corp.com>
2019-02-18 14:36:59 +0900

    SQL문 변경

--- src/main/java/com/nts/reservation/dao/sql/MyReservationSqls.java
+++ src/main/java/com/nts/reservation/dao/sql/MyReservationSqls.java
@@ -7,17 +7,20 @@ package com.nts.reservation.dao.sql;
 public class MyReservationSqls {
 	public static final String SELECT_MY_RESERVATION_INFO =
 		"SELECT" + 
+		" reservation_info.id AS reservation_info_id," + 
+		" reservation_info_price.id AS reservation_info_price_id," + 
 		" reservation_info.reservation_name," + 
 		" reservation_info.display_info_id," + 
-		" product.description AS product_description," +
+		" product.description AS product_description," + 
 		" display_info.place_name," + 
 		" display_info.opening_hours," + 
-		" ROUND(product_price.price * (100-product_price.discount_rate) * reservation_info_price.count/100,0) AS price," + 
+		" SUM(ROUND(product_price.price * (100-product_price.discount_rate) * reservation_info_price.count/100,0)) AS price," + 
 		" reservation_info.cancel_flag" + 
 		" FROM reservation_info" + 
 		" INNER JOIN display_info ON reservation_info.display_info_id = display_info.id" + 
 		" INNER JOIN product ON reservation_info.product_id = product.id" + 
 		" INNER JOIN reservation_info_price ON reservation_info.id = reservation_info_price.reservation_info_id" + 
 		" INNER JOIN product_price ON reservation_info_price.product_price_id = product_price.id" + 
-		" WHERE reservation_info.reservation_email = :reservationEmail";
+		" WHERE reservation_info.reservation_email = :reservationEmail" + 
+		" GROUP BY reservation_info_id;";
 }
