c92528d821298a39a12194bc88483cd8e39b88ca
yongjoon.Park <yongjoon.Park@nts-corp.com>
2019-02-18 16:43:58 +0900

    enum적용

--- src/main/java/com/nts/reservation/product/dto/ImageType.java
+++ src/main/java/com/nts/reservation/product/dto/ImageType.java
@@ -4,29 +4,18 @@
  */
 package com.nts.reservation.product.dto;
 
-import java.util.Arrays;
-
 public enum ImageType {
 	th("th"),
 	ma("ma"),
 	et("et");
-	
+
 	private final String imageType;
-	
+
 	private ImageType(String imageType) {
 		this.imageType = imageType;
 	}
-	
+
 	public String getImageType() {
 		return imageType;
-	}
-	
-	/**
-	 * Image type이 Enum에 존재하는 지 확인
-	 * @param type image type
-	 */
-	public static boolean isExistImageType(String type) {
-		return Arrays.asList(ImageType.values()).stream()
-			.anyMatch(todoStatus -> todoStatus.getImageType().equals(type));
 	}
 }
