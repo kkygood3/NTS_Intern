3605c19f27cd397423661b70dd77d9236f4fb827
박우성 <woosung.park@nts-corp.com>
2019-02-18 18:07:41 +0900

    ajaxRequest function의 parameter 추가

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -18,14 +18,19 @@ const PRICE_TYPE ={
  * 반복적으로 호출하는 XMLHttpRequest를 전담하는 함수
  * @param 페이지 로드후에 실행할 callback function. 매개변수로 response를 가져아 함 
  * @param api URL
+ * @method GET, POST, PUT, DELETE
  */
-function requestAjax(callback, url) {
+
+function requestAjax(callback, url, method) {
+	if(!method){
+		method = 'GET';
+	}
 	var ajaxReq = new XMLHttpRequest();
 	ajaxReq.callback = callback;
 	ajaxReq.addEventListener('load', function(evt) {
 		this.callback(evt.target.response);
 	});
-	ajaxReq.open('GET', url);
+	ajaxReq.open(method, url);
 	ajaxReq.responseType = 'json';
 	ajaxReq.send()
 }
