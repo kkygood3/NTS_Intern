4a9723affefde5a4d367c99aa62b491a970e6950
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-19 18:41:52 +0900

    Main Controller 분류, login/logout 구현

--- src/main/java/com/nts/reservation/common/controller/MainController.java
+++ src/main/java/com/nts/reservation/common/controller/MainController.java
@@ -4,51 +4,42 @@
  */
 package com.nts.reservation.common.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.nts.reservation.display.dto.DisplayResponse;
-import com.nts.reservation.display.service.DisplayService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-
-	@Autowired
-	DisplayService displayService;
 
 	@GetMapping(path = "/main")
 	public String goMain() {
 		return "mainpage";
 	}
 
-	@GetMapping(path = "/review/{displayInfoId}")
-	public ModelAndView goReview(@PathVariable(name = "displayInfoId") int displayInfoId) {
-		ModelAndView modelAndView = new ModelAndView("review");
-		modelAndView.addObject("displayInfoId", displayInfoId);
-		return modelAndView;
+	@GetMapping(path = "/login")
+	public String goLogin(HttpSession session) {
+		if (session.getAttribute("email") != null) {
+			return "redirect:/myreservation";
+		}
+		return "bookinglogin";
 	}
 
-	@GetMapping(path = "/display/detail/{displayInfoId}")
-	public ModelAndView goDetail(@PathVariable(name = "displayInfoId") int displayInfoId) {
-		ModelAndView modelAndView = new ModelAndView("detail");
-		modelAndView.addObject("displayInfoId", displayInfoId);
-		return modelAndView;
-	}
+	@PostMapping(path = "/login")
+	public String login(@RequestParam(name = "reservation_email") String email, HttpSession session) {
 
-	@GetMapping(path = "/reserve/{displayInfoId}")
-	public ModelAndView goReserve(@PathVariable(name = "displayInfoId") int displayInfoId) {
-		ModelAndView modelAndView = new ModelAndView("reserve");
-		DisplayResponse displayResponse = displayService.getDisplayInfo(displayInfoId, 0);
-		modelAndView.addObject("displayResponse", displayResponse);
-		return modelAndView;
-	}
+		// TODO email validation check
 
-	@GetMapping(path = "/myReserve")
-	public String goMyReserve() {
+		session.setAttribute("email", email);
 		return "myreservation";
+	}
+
+	@GetMapping(path = "/logout")
+	public String logoutTest(HttpSession session) {
+		session.removeAttribute("email");
+		return "redirect:/";
 	}
 
 	@GetMapping(path = "/error")
