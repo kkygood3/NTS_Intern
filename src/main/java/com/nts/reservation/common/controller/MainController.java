ee53b34c378e63481e7cc69c6018ef7d4f25042e
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-19 11:04:21 +0900

    경로 수정 (@RequestParam -> @PathVariable)

--- src/main/java/com/nts/reservation/common/controller/MainController.java
+++ src/main/java/com/nts/reservation/common/controller/MainController.java
@@ -4,33 +4,53 @@
  */
 package com.nts.reservation.common.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.nts.reservation.display.dto.DisplayResponse;
+import com.nts.reservation.display.service.DisplayService;
 
 @Controller
 public class MainController {
+
+	@Autowired
+	DisplayService displayService;
 
 	@GetMapping(path = "/main")
 	public String goMain() {
 		return "mainpage";
 	}
 
-	@GetMapping(path = "/review")
-	public ModelAndView goReview(@RequestParam(name = "displayInfoId") int displayInfoId) {
+	@GetMapping(path = "/review/{displayInfoId}")
+	public ModelAndView goReview(@PathVariable(name = "displayInfoId") int displayInfoId) {
 		ModelAndView modelAndView = new ModelAndView("review");
 		modelAndView.addObject("displayInfoId", displayInfoId);
 		return modelAndView;
 	}
 
-	@GetMapping(path = "/detail")
-	public ModelAndView goDetail(@RequestParam(name = "displayInfoId") int displayInfoId) {
+	@GetMapping(path = "/display/detail/{displayInfoId}")
+	public ModelAndView goDetail(@PathVariable(name = "displayInfoId") int displayInfoId) {
 		ModelAndView modelAndView = new ModelAndView("detail");
 		modelAndView.addObject("displayInfoId", displayInfoId);
 		return modelAndView;
 	}
-	
+
+	@GetMapping(path = "/reserve/{displayInfoId}")
+	public ModelAndView goReserve(@PathVariable(name = "displayInfoId") int displayInfoId) {
+		ModelAndView modelAndView = new ModelAndView("reserve");
+		DisplayResponse displayResponse = displayService.getDisplayInfo(displayInfoId, 0);
+		modelAndView.addObject("displayResponse", displayResponse);
+		return modelAndView;
+	}
+
+	@GetMapping(path = "/myReserve")
+	public String goMyReserve() {
+		return "myreservation";
+	}
+
 	@GetMapping(path = "/error")
 	public String goError() {
 		return "common/error";
