1ae11a3b935cd24559f3be74aaf8152be1e493f9
배진우 <jinwoo.bae@nts-corp.com>
2019-02-19 18:40:01 +0900

    트랜잭션 애노테이션 추가

--- src/main/java/com/nts/reservation/service/impl/ReservationServiceImpl.java
+++ src/main/java/com/nts/reservation/service/impl/ReservationServiceImpl.java
@@ -57,6 +57,7 @@ public class ReservationServiceImpl implements ReservationService {
 	}
 
 	@Override
+	@Transactional
 	public void cancleReservation(int reservationId) {
 		reservationInfoDao.updateReservationToCancel(reservationId);
 	}
