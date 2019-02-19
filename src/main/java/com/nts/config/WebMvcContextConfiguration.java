5becd7dbafee49182bf6fae3502dda2e80129cbc
최석현 <lkamna12@nts-corp.com>
2019-02-19 19:33:09 +0900

    GlobalExceptionController 위치 및 이름 변경

--- src/main/java/com/nts/config/WebMvcContextConfiguration.java
+++ src/main/java/com/nts/config/WebMvcContextConfiguration.java
@@ -8,6 +8,7 @@ package com.nts.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -31,6 +32,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.nts.controller" })
+@Import(ControllerExceptionHandlerConfig.class)
 public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
 	
 	private static final int YEAR_SECONDS = 31556926;
