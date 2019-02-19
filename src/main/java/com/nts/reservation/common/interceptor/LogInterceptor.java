745b378339bc96ac32cce1d5d51748a8a2b10d5e
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-19 18:43:07 +0900

    class name 변경 (Interceptor ->LogInterceptor)

--- src/main/java/com/nts/reservation/common/interceptor/LogInterceptor.java
+++ src/main/java/com/nts/reservation/common/interceptor/LogInterceptor.java
@@ -8,20 +8,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class Interceptor extends HandlerInterceptorAdapter {
+public class LogInterceptor extends HandlerInterceptorAdapter {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-
+	// TODO logger -> 왜 static final ?
+	// TODO logger와 interceptor (interceptor class name 변경하기)
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 		throws Exception {
-		logger.debug("=== {} 호출 ===", handler.toString());
+		logger.debug("preHandle /\t호출 : {}", handler.toString());
 		return true;
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 		ModelAndView modelAndView) throws Exception {
-		logger.debug("=== MdeolAndView : {},  Handler : {} ===", modelAndView, handler.toString());
+		logger.debug("postHandle /\tModelAndView : {},  Handler : {}", modelAndView, handler.toString());
 	}
 
 }
