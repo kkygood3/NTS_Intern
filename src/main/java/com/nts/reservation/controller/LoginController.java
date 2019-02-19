dc9e7021970766c10b5fd331fb686398d28765be
siyoon <c.yun@nts-corp.com>
2019-02-19 14:33:08 +0900

    be input validation

--- src/main/java/com/nts/reservation/controller/LoginController.java
+++ src/main/java/com/nts/reservation/controller/LoginController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.nts.reservation.service.validation.Validation;
+
 @Controller
 public class LoginController {
 	@GetMapping(path="/login")
@@ -17,6 +19,9 @@ public class LoginController {
 	@PostMapping(path="/login")
 	public String postLogin(@RequestParam(name="email", required=true) String email,
 		HttpSession session) {
+		if (!Validation.getInstance().validateEmail(email)) {
+			return "redirect:/error";
+		}
 		session.setAttribute("email", email);
 		return "redirect:/reservation";
 	}
