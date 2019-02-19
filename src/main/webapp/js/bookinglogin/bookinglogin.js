83536b0016fac7cad64d3f166a6770d0cbcb436d
siyoon <c.yun@nts-corp.com>
2019-02-19 14:34:36 +0900

    불필요한 코드 삭제

--- src/main/webapp/js/bookinglogin/bookinglogin.js
+++ src/main/webapp/js/bookinglogin/bookinglogin.js
@@ -4,12 +4,6 @@
  *
  */
 
-function RegularExpression() {
-	this.name = /^([가-힣]{2,})$|^(([A-Z][a-z]*\s)+[A-Z][a-z]*)$/;
-	this.tel = /^01[016-9]-[0-9]{3,4}-[0-9]{4}$/;
-	this.email = /^[a-z][-\.\w]*@[a-z][-\.\w]*\.[a-z]{2,3}$/i;
-}
-
 document.addEventListener("DOMContentLoaded", function() {
 	var input = document.querySelector("form input");
 	var submitButton = document.querySelector("button.login_btn.confirm");
