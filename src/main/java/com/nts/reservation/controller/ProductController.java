40dd55815baca314519707fbb0a88b9f3efa530b
siyoon <c.yun@nts-corp.com>
2019-02-19 15:04:20 +0900

    예외처리 수정

--- src/main/java/com/nts/reservation/controller/ProductController.java
+++ src/main/java/com/nts/reservation/controller/ProductController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.nts.reservation.controller.ErrorController.ErrorInfo;
 import com.nts.reservation.dto.CommentPageInfo;
+import com.nts.reservation.dto.ErrorInfo;
 import com.nts.reservation.dto.ProductPageInfo;
 import com.nts.reservation.dto.ReservationPageInfo;
 import com.nts.reservation.dto.UserReservationInput;
@@ -36,7 +36,7 @@ public class ProductController {
 	private CommentService commentService;
 	@Autowired
 	private ReservationService reservationService;
-	
+
 	/**
 	 * 디테일 페이지에 표시할 정보 담아서 url맵핑한다
 	 * @param displayInfoId 조회할 상품 정보
@@ -77,33 +77,36 @@ public class ProductController {
 	 * @return 뷰이름 리턴
 	 */
 	@GetMapping(path = "/{displayInfoId}/reservation")
-	public String getReservation(@PathVariable(name="displayInfoId", required= true) long displayInfoId,
+	public String getReservation(@PathVariable(name = "displayInfoId", required = true) long displayInfoId,
 		ModelMap model) {
-		ReservationPageInfo reservationPageInfo = reservationService.getReservationPageInfoByDisplayInfoId(displayInfoId);
+		ReservationPageInfo reservationPageInfo = reservationService
+			.getReservationPageInfoByDisplayInfoId(displayInfoId);
 		model.addAttribute("pageInfo", reservationPageInfo);
 		return "reservation";
 	}
-	
+
 	/**
-	 * 예약페이지에 표시할 정보 담아서 url맵핑한다
-	 * @param displayInfoId 조회할 상품
-	 * @param model 표시할 정보를 담는다
-	 * @return 뷰이름 리턴
+	 * 예약정보 받아서 서버로 넘긴다
+	 * @param displayInfoId 예약할 상품 id
+	 * @param userReservationInputString 예약정보 
+	 * @param model
+	 * @return
 	 */
 	@PostMapping(path = "/{displayInfoId}/reservation")
-	public String postReservation(@PathVariable(name="displayInfoId", required= true) long displayInfoId,
-		@RequestParam (name = "user_reservation_input", required = true) String userReservationInputString,
+	public String postReservation(@PathVariable(name = "displayInfoId", required = true) long displayInfoId,
+		@RequestParam(name = "user_reservation_input", required = true) String userReservationInputString,
 		ModelMap model) {
-		
 		ObjectMapper mapper = new ObjectMapper();
 		UserReservationInput userReservationInput = null;
 		try {
 			userReservationInput = mapper.readValue(userReservationInputString, UserReservationInput.class);
 		} catch (IOException e) {
+			model.addAttribute("errorInfo", new ErrorInfo(500, "Internal Server Error", e.getMessage()));
 			return "error";
 		}
-		
+
 		if (reservationService.addReservation(userReservationInput, displayInfoId) == null) {
+			model.addAttribute("errorInfo", new ErrorInfo(400, "BAD_REQUEST", "잘못된 입력입니다."));
 			return "error";
 		}
 		return "redirect:/detail/" + displayInfoId;
