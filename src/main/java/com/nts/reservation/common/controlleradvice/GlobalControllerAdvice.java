ffb738a85bb777ed6fa1432451cd8a191ab5a954
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-19 18:43:56 +0900

    package name 변경, 중복되는 log 삭제

--- src/main/java/com/nts/reservation/common/controlleradvice/GlobalControllerAdvice.java
+++ src/main/java/com/nts/reservation/common/controlleradvice/GlobalControllerAdvice.java
@@ -2,10 +2,8 @@
  * Copyright 2019 Naver Corp. All rights Reserved.
  * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
-package com.nts.reservation.common.controllerAdvice;
+package com.nts.reservation.common.controlleradvice;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,15 +12,10 @@ import org.springframework.web.servlet.ModelAndView;
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
 	@ExceptionHandler(value = IllegalArgumentException.class)
 	public ModelAndView handleException400(IllegalArgumentException e) {
-		logger.warn("!warn : {}", e.getMessage());
-		e.printStackTrace();
-
+		// XXX logger level을 정하는 기준?
 		ModelAndView modelAndView = new ModelAndView("common/error");
-		modelAndView.addObject("errorMessage", "BAD_REQUEST!! Please try agin...");
 		modelAndView.setStatus(HttpStatus.BAD_REQUEST);
 		return modelAndView;
 	}
