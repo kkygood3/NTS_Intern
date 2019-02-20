48dd8f7b72defe0c47689b391813446c7660c424
배진우 <jinwoo.bae@nts-corp.com>
2019-02-20 15:49:28 +0900

    정규식 오타 수정

--- src/main/java/com/nts/reservation/constant/RegularExpression.java
+++ src/main/java/com/nts/reservation/constant/RegularExpression.java
@@ -11,5 +11,5 @@ package com.nts.reservation.constant;
 public class RegularExpression {
 	public static final String NAME_REGEXP = "^[가-힣|a-z|A-Z\\s]{1,17}$";
 	public static final String PHONE_REGEXP = "^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}$";
-	public static final String EMAIL_REGEXP = "(?=^.{1,50}$)	^([\\w-]+(?:\\.[\\w-]+)*)@((?:[\\w-]+\\.)*\\w[\\w-]{0,46})\\.([a-z]{2,6}(?:\\.[a-z]{2})?)$";
+	public static final String EMAIL_REGEXP = "(?=^.{1,50}$)^([\\w-]+(?:\\.[\\w-]+)*)@((?:[\\w-]+\\.)*\\w[\\w-]{0,46})\\.([a-z]{2,6}(?:\\.[a-z]{2})?)$";
 }
