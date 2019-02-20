473134e0cec675c006841cc621838c526251a0db
박우성 <woosung.park@nts-corp.com>
2019-02-20 16:39:45 +0900

    불필요한 코드 삭제

--- src/main/java/com/nts/reservation/interceptor/SessionInterceptor.java
+++ src/main/java/com/nts/reservation/interceptor/SessionInterceptor.java
@@ -2,7 +2,6 @@ package com.nts.reservation.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -11,11 +10,6 @@ public class SessionInterceptor extends HandlerInterceptorAdapter{
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 		ModelAndView modelAndView) throws Exception {
-		HttpSession session = request.getSession();
-		
-		String email = (String)session.getAttribute("email");
-		if (email != null) {
-			request.setAttribute("email", email);
-		}
+		request.setAttribute("email", request.getSession().getAttribute("email"));
 	}
 }
