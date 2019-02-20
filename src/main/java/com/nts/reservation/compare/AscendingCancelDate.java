82c080d843350ec434add507ab1dbcd5449875b5
siyoon <c.yun@nts-corp.com>
2019-02-20 16:08:18 +0900

    주석추가, 로직 변경

--- src/main/java/com/nts/reservation/compare/AscendingCancelDate.java
+++ src/main/java/com/nts/reservation/compare/AscendingCancelDate.java
@@ -4,11 +4,16 @@ import java.util.Comparator;
 
 import com.nts.reservation.dto.ReservationDisplayItem;
 
+/**
+ * 최근에 취소한 순서로 정렬 
+ * @author si yoon
+ *
+ */
 public class AscendingCancelDate implements Comparator<ReservationDisplayItem> {
 	@Override
 	public int compare(ReservationDisplayItem r1, ReservationDisplayItem r2) {
 		long d1 = r1.getModifyDate().getTime();
 		long d2 = r2.getModifyDate().getTime();
-		return (int)(d1 - d2);
+		return (int)(d2 - d1);
 	}
 }
