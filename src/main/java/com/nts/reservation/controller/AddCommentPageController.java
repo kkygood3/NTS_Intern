2063234e62e05f6d78fffeed48475b28e327c257
Yook Sung Ryul <yook0328@gmail.com>
2019-02-20 16:47:24 +0900

    AddCommentPageController 추가.

---
+++ src/main/java/com/nts/reservation/controller/AddCommentPageController.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright 2019 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.nts.reservation.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.nts.reservation.dto.ReservationInfoDto;
+import com.nts.reservation.service.ReservationService;
+
+/**
+ * @author 육성렬
+ */
+@Controller
+public class AddCommentPageController {
+
+	@Autowired
+	private ReservationService reservationService;
+
+	@GetMapping("/addComment/{reservationInfoId}")
+	public String getAddCommentPage(@PathVariable Long reservationInfoId, ModelMap map) {
+		ReservationInfoDto reservation = reservationService.getReservation(reservationInfoId);
+		map.addAttribute("reservation", reservation);
+		return "addCommentPage";
+	}
+}
