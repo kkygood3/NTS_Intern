bbf25e86275330cab99e5f0a57cb6a661472ccc3
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-20 18:50:08 +0900

    Constructor 추가

--- src/main/java/com/nts/reservation/reserve/dto/ReservationParam.java
+++ src/main/java/com/nts/reservation/reserve/dto/ReservationParam.java
@@ -2,13 +2,15 @@ package com.nts.reservation.reserve.dto;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter @Setter @ToString
-@Builder
+@Builder @NoArgsConstructor @AllArgsConstructor
 public class ReservationParam {
 
 	private int displayInfoId;
