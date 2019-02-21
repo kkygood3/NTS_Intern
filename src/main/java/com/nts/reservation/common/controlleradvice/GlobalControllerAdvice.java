e84088cb0098e401f9b440245f24469ff58dbd2c
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-21 18:20:08 +0900

    Exception Handler 추가 (405)

--- src/main/java/com/nts/reservation/common/controlleradvice/GlobalControllerAdvice.java
+++ src/main/java/com/nts/reservation/common/controlleradvice/GlobalControllerAdvice.java
@@ -5,6 +5,7 @@
 package com.nts.reservation.common.controlleradvice;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,7 +17,15 @@ public class GlobalControllerAdvice {
 	public ModelAndView handleException400(IllegalArgumentException e) {
 		// XXX logger level을 정하는 기준?
 		ModelAndView modelAndView = new ModelAndView("common/error");
+		modelAndView.addObject("errorMessage", e.getMessage());
 		modelAndView.setStatus(HttpStatus.BAD_REQUEST);
+		return modelAndView;
+	}
+
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public ModelAndView handleError405(HttpRequestMethodNotSupportedException e) {
+		ModelAndView modelAndView = new ModelAndView("common/error");
+		modelAndView.addObject("errorMessage", "지원하지 않는 요청입니다.");
 		return modelAndView;
 	}
 }
