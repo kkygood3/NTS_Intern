4c7714948d6be0b3e95b78e17ba921d0905369f4
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-21 15:58:14 +0900

    주석 수정

--- src/main/java/com/nts/reservation/common/interceptor/LogInterceptor.java
+++ src/main/java/com/nts/reservation/common/interceptor/LogInterceptor.java
@@ -10,8 +10,6 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class LogInterceptor extends HandlerInterceptorAdapter {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	// TODO logger -> 왜 static final ?
-	// TODO logger와 interceptor (interceptor class name 변경하기)
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
