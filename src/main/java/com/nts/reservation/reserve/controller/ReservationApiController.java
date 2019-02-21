792c111e70ba0316bef36cf5119d9a10b4481aef
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-21 17:09:09 +0900

    Valid annotation 적용, 예매 취소 예외상황, addHyphenToPhone method 작성

--- src/main/java/com/nts/reservation/reserve/controller/ReservationApiController.java
+++ src/main/java/com/nts/reservation/reserve/controller/ReservationApiController.java
@@ -4,9 +4,15 @@
  */
 package com.nts.reservation.reserve.controller;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,34 +27,66 @@ import com.nts.reservation.reserve.service.ReservationService;
 @RestController
 @RequestMapping(path = "/api")
 public class ReservationApiController {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(ReservationApiController.class);
-	
+
+	private static final int NOT_UPDATED = 0;
+
 	@Autowired
 	ReservationService reservationService;
 
-	// TODO A, Y, B 등 type enum 처리
+	/**
+	 * 입력 받은 정보를 등록한 후, 등록된 예약 정보를 반환
+	 * @param reservationParam 각 양식에 맞는 예약 정보
+	 * @return
+	 */
 	@PostMapping(path = "/reservations")
-	public ReservationResponse reserve(@RequestBody ReservationParam reservationParam) {
-		// TODO ReservationParam -> displayInfoId, reservationEmail, reservationName, reservationTel, ReservationPrice
-			// ReservationPrice -> count, productPriceId
-		
-		// validate param (use StringUtils) 효율적인 방법 찾아보기
-		
-		// ReservationService (@Transactional(readOnly=false) 사용) -> readOnly 기능 다시한번 보기
-		
-		return reservationService.insertAndReturnRerservationParam(reservationParam);
-	}
-	
-	@PutMapping(path = "/reservations/{reservationId}")
-	public ReservationResponse cancelReservation(@PathVariable(name="reservationId") int reservationId) {
-		int result = reservationService.cancelReservation(reservationId);
-		
-		if(result != 1) {
-			// TODO exception?
-			LOGGER.warn("Update Fail!! / reservationId : {}", reservationId);
+	public ReservationResponse reserve(@RequestBody @Valid ReservationParam reservationParam, BindingResult result,
+		HttpServletResponse response) {
+
+		if (result.hasErrors()) {
+			LOGGER.debug("{} / field : {} / code : {}", result.getFieldError().getDefaultMessage(),
+				result.getFieldError().getField(), result.getFieldError().getCode());
+			response.setStatus(HttpStatus.BAD_REQUEST.value());
+			return ReservationResponse.builder().build();
 		}
+		String appliedStandardPhoneForm = addHyphenToPhone(reservationParam.getReservationTel());
+		reservationParam.setReservationTel(appliedStandardPhoneForm);
 		
+		int reservationInfoId = reservationService.insertReservation(reservationParam); 
+		return reservationService.getReservationResponse(reservationInfoId);
+	}
+
+	@PutMapping(path = "/reservations/{reservationId}")
+	public ReservationResponse cancelReservation(@PathVariable(name = "reservationId") int reservationId,
+		HttpSession session, HttpServletResponse response) {
+
+		String reservationEmail = reservationService.getReservationResponse(reservationId).getReservationEmail();
+		String email = (String)session.getAttribute("email");
+
+		if (!reservationEmail.equals(email)) {
+			response.setStatus(HttpStatus.BAD_REQUEST.value());
+			return ReservationResponse.builder().build();
+		}
+
+		int updateResult = reservationService.cancelReservation(reservationId);
+		if (updateResult == NOT_UPDATED) {
+			IllegalArgumentException e = new IllegalArgumentException("Does not exist reservationId!!");
+			LOGGER.warn("Bad Request! Parameter / Error Message : {} / reservationId : {}", e.getMessage(),
+				reservationId, e);
+			throw e;
+		}
+
 		return reservationService.getReservationResponse(reservationId);
+	}
+
+	/**
+	 * 입력받은 전화번호를 양식에 맞게 적용
+	 */
+	private String addHyphenToPhone(String phone) {
+		if (phone.length() == 10) {
+			return phone.substring(0, 3) + "-" + phone.substring(3, 6) + "-" + phone.substring(6);
+		}
+		return phone.substring(0, 3) + "-" + phone.substring(3, 7) + "-" + phone.substring(7);
 	}
 }
