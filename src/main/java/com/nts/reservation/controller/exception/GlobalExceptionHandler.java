6a3972b1feccdc668974ff108a6494287d31526f
배진우 <jinwoo.bae@nts-corp.com>
2019-02-21 18:29:12 +0900

    import 정리

--- src/main/java/com/nts/reservation/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nts/reservation/controller/exception/GlobalExceptionHandler.java
@@ -7,11 +7,9 @@ package com.nts.reservation.controller.exception;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
