419948ac5575ff6d94ed2e2895a6ad36fc2f0bcc
siyoon <c.yun@nts-corp.com>
2019-02-21 13:37:00 +0900

    jdbc->mybatis

--- src/main/java/com/nts/reservation/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/nts/reservation/service/impl/CategoryServiceImpl.java
@@ -6,19 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.nts.reservation.dao.CategoryDao;
+import com.nts.reservation.dao.CategoryMapper;
 import com.nts.reservation.dto.Category;
 import com.nts.reservation.service.CategoryService;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
 	@Autowired
-	private CategoryDao promotionDao;
+	private CategoryMapper categoryDao;
 
 	@Override
 	@Transactional
 	public List<Category> getCategoris() {
-		List<Category> list = promotionDao.selectAll();
+		List<Category> list = categoryDao.selectAllCategoris();
 		return list;
 	}
 }
