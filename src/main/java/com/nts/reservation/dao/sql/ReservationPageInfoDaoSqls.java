f092cb43695056a4ecc6293479de169970b63d25
siyoon <c.yun@nts-corp.com>
2019-02-21 13:20:55 +0900

    .

--- src/main/java/com/nts/reservation/dao/sql/ReservationPageInfoDaoSqls.java
+++ src/main/java/com/nts/reservation/dao/sql/ReservationPageInfoDaoSqls.java
@@ -8,7 +8,7 @@ public class ReservationPageInfoDaoSqls {
 	+ "JOIN product_image AS pi ON pi.type = 'ma' AND p.id = pi.product_id "
 	+ "JOIN file_info AS pf ON pf.id = pi.file_id";
 	
-	public static final String SELECT_RESERVATION_DISPLAY_LIST_ITEM_BY_RESERVATION_EMAIL_WITH_CONDITION
+	public static final String SELECT_RESERVATION_DISPLAY_LIST_ITEM_BY_RESERVATION_EMAIL
 	= "SELECT r.id AS reservation_id, p.description, r.reservation_date, r.cancel_flag AS canceled, d.place_name, sum(pp.price*rp.count) AS price, r.modify_date "
 	+ "FROM reservation_info AS r "
 	+ "INNER JOIN product AS p "
@@ -17,7 +17,6 @@ public class ReservationPageInfoDaoSqls {
 	+ "INNER JOIN display_info AS d ON r.display_info_id = d.id "
 	+ "INNER JOIN reservation_info_price AS rp ON rp.reservation_info_id = r.id "
 	+ "INNER JOIN product_price AS pp ON rp.product_price_id = pp.id "
-	+ ":condition "
 	+ "GROUP BY r.id "
 	+ "LIMIT :start, :limit";
 	
