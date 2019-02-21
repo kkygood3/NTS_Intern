d0f0a1bc7b93cc413bf0cb46d0ebe2e09adf3fbc
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-21 16:01:38 +0900

    로그인 시 validation check, 예외 상황 추가

--- src/main/java/com/nts/reservation/common/controller/MainController.java
+++ src/main/java/com/nts/reservation/common/controller/MainController.java
@@ -4,12 +4,17 @@
  */
 package com.nts.reservation.common.controller;
 
+import java.util.regex.Pattern;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.nts.reservation.common.utils.RegexPattern;
 
 @Controller
 public class MainController {
@@ -28,12 +33,24 @@ public class MainController {
 	}
 
 	@PostMapping(path = "/login")
-	public String login(@RequestParam(name = "reservation_email") String email, HttpSession session) {
+	public ModelAndView login(
+		@RequestParam(name = "reservation_email") String email, HttpSession session, ModelAndView modelAndView) {
+		
+		modelAndView.setViewName("redirect:/myreservation");
 
-		// TODO email validation check
+		String sessionEmail = (String)session.getAttribute("email");
+		if (sessionEmail != null) {
+			return modelAndView;
+		}
+
+		if (checkValidation(email) == false) {
+			modelAndView.setViewName("redirect:/login");
+			modelAndView.addObject("errorMessage", "email 형식이 올바르지 않습니다.");
+			return modelAndView;
+		}
 
 		session.setAttribute("email", email);
-		return "redirect:/myreservation";
+		return modelAndView;
 	}
 
 	@GetMapping(path = "/logout")
@@ -45,5 +62,18 @@ public class MainController {
 	@GetMapping(path = "/error")
 	public String goError() {
 		return "common/error";
+	}
+
+	private boolean checkValidation(String email) {
+		if (email == null) {
+			return false;
+		}
+		if (email.length() == 0) {
+			return false;
+		}
+		if (!Pattern.matches(RegexPattern.EMAIL_REGEX, email)) {
+			return false;
+		}
+		return true;
 	}
 }
