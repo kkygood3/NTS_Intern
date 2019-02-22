bf28c68f380d62da6006f3317605fdfa0088c70c
이승수 <lee-seungsoo@nts-corp.com>
2019-02-22 16:29:09 +0900

    api controller 분리

--- src/main/java/com/nts/reservation/controller/ReservationApiController.java
+++ src/main/java/com/nts/reservation/controller/ReservationApiController.java
@@ -5,7 +5,6 @@
 package com.nts.reservation.controller;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,22 +23,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.nts.reservation.dto.Category;
 import com.nts.reservation.dto.Comment;
 import com.nts.reservation.dto.CommentImage;
-import com.nts.reservation.dto.DisplayInfo;
-import com.nts.reservation.dto.DisplayInfoImage;
 import com.nts.reservation.dto.FileInfo;
-import com.nts.reservation.dto.Product;
-import com.nts.reservation.dto.ProductImage;
-import com.nts.reservation.dto.ProductPrice;
 import com.nts.reservation.dto.Reservation;
 import com.nts.reservation.dto.ReservedItem;
-import com.nts.reservation.service.CategoryService;
 import com.nts.reservation.service.CommentService;
-import com.nts.reservation.service.DetailProductService;
 import com.nts.reservation.service.FileIoService;
-import com.nts.reservation.service.ProductService;
 import com.nts.reservation.service.ReservationService;
 
 /**
@@ -49,72 +39,11 @@ import com.nts.reservation.service.ReservationService;
 @RequestMapping(path = "/api")
 public class ReservationApiController {
 	@Autowired
-	private ProductService productService;
-	@Autowired
-	private CategoryService categoryService;
-	@Autowired
-	private DetailProductService detailProductService;
-	@Autowired
 	private ReservationService reservationService;
 	@Autowired
 	private CommentService commentService;
 	@Autowired
 	private FileIoService fileIoService;
-
-	@GetMapping(path = "/products")
-	public Map<String, Object> getProducts(@RequestParam(name = "categoryId", required = false) Integer categoryId,
-		@RequestParam(name = "start", required = false, defaultValue = "0") int start) {
-		List<Product> products = productService.getProducts(categoryId, start);
-		int countProducts = productService.getCount(categoryId);
-
-		Map<String, Object> map = new HashMap<>();
-		map.put("items", products);
-		map.put("totalCount", countProducts);
-
-		return map;
-	}
-
-	@GetMapping(path = "/products/{displayInfoId}")
-	public Map<String, Object> getDetail(@PathVariable("displayInfoId") Integer displayInfoId) {
-		BigDecimal averageScore = detailProductService.getAverageScore(displayInfoId);
-		List<Comment> comments = detailProductService.getComments(displayInfoId);
-		DisplayInfo displayInfo = detailProductService.getDisplayInfo(displayInfoId);
-		DisplayInfoImage displayInfoImage = detailProductService.getDisplayInfoImage(displayInfoId);
-		List<ProductImage> productImages = detailProductService.getProductImages(displayInfoId);
-		List<ProductPrice> productPrices = detailProductService.getProductPrices(displayInfoId);
-
-		Map<String, Object> map = new HashMap<>();
-		map.put("averageScore", averageScore);
-		map.put("comments", comments);
-		map.put("displayInfo", displayInfo);
-		map.put("displayInfoImage", displayInfoImage);
-		map.put("productImages", productImages);
-		map.put("productPrices", productPrices);
-
-		return map;
-	}
-
-	@GetMapping(path = "/promotions")
-	public Map<String, Object> getPromotions() {
-		List<Product> promotions = productService.getPromotions();
-		int countProducts = productService.getCountPromotions();
-
-		Map<String, Object> map = new HashMap<>();
-		map.put("items", promotions);
-		map.put("totalCount", countProducts);
-
-		return map;
-	}
-
-	@GetMapping(path = "/categories")
-	public Map<String, Object> getCategories() {
-		List<Category> categories = categoryService.getCategories();
-
-		Map<String, Object> map = new HashMap<>();
-		map.put("items", categories);
-
-		return map;
-	}
 
 	@GetMapping(path = "/reservations")
 	public Map<String, Object> getReservations(@RequestParam String reservationEmail) {
