d08d7912b2c0a88d2c24210c52c7268aa4fdada5
yongjoon.Park <yongjoon.Park@nts-corp.com>
2019-02-22 16:14:27 +0900

    http status 추가

--- src/main/java/com/nts/reservation/common/controlleradvice/GlobalControllerAdvice.java
+++ src/main/java/com/nts/reservation/common/controlleradvice/GlobalControllerAdvice.java
@@ -8,21 +8,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
 	@ExceptionHandler(value = IllegalArgumentException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ModelAndView handleException400(IllegalArgumentException e) {
 		// XXX logger level을 정하는 기준?
 		ModelAndView modelAndView = new ModelAndView("common/error");
 		modelAndView.addObject("errorMessage", e.getMessage());
-		modelAndView.setStatus(HttpStatus.BAD_REQUEST);
 		return modelAndView;
 	}
 
 	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	@ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
 	public ModelAndView handleError405(HttpRequestMethodNotSupportedException e) {
 		ModelAndView modelAndView = new ModelAndView("common/error");
 		modelAndView.addObject("errorMessage", "지원하지 않는 요청입니다.");
