07386bd0e411a516f47dd8ccba6dc72c890ca221
배진우 <jinwoo.bae@nts-corp.com>
2019-02-22 11:16:55 +0900

    주석추가

--- src/main/java/com/nts/reservation/argumentresolver/PageDtoArgumentResolver.java
+++ src/main/java/com/nts/reservation/argumentresolver/PageDtoArgumentResolver.java
@@ -21,8 +21,8 @@ import com.nts.reservation.dto.param.PageDto;
  */
 public class PageDtoArgumentResolver implements HandlerMethodArgumentResolver {
 
-	/* 
-	 * 
+	/**
+	 * 파라미터 타입이 PageDto.class 일경우에만 resolveArgument 메소드가 호출되어집니다.
 	 */
 	@Override
 	public boolean supportsParameter(MethodParameter parameter) {
@@ -31,15 +31,17 @@ public class PageDtoArgumentResolver implements HandlerMethodArgumentResolver {
 	}
 
 	/* 
-	 * 
+	 * 페이징 처리용 start, limit값을 적절히 입력해줍니다.
+	 * PageDefault 애노테이션이 파라미터 앞에 지정되어져있는 경우 이값을 Default로 지정해줍니다.
+	 * WebRequest 파라미터에 start, limit값이 있는경우 이값을 넣어줍니다.
 	 */
 	@Override
 	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
 		NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-		
-		int start = Integer.parseInt(DEFAULT_START); 
+
+		int start = Integer.parseInt(DEFAULT_START);
 		int limit = Integer.parseInt(DEFAULT_LIMIT);
-		
+
 		if (parameter.hasParameterAnnotation(PageDefault.class)) {
 			PageDefault pageDefault = parameter.getParameterAnnotation(PageDefault.class);
 			start = Integer.parseInt(pageDefault.start());
@@ -49,11 +51,11 @@ public class PageDtoArgumentResolver implements HandlerMethodArgumentResolver {
 		if (webRequest.getParameter("start") != null) {
 			start = Integer.parseInt(webRequest.getParameter("start"));
 		}
-		
+
 		if (webRequest.getParameter("limit") != null) {
 			limit = Integer.parseInt(webRequest.getParameter("limit"));
 		}
-		
+
 		return new PageDto(start, limit);
 	}
 
