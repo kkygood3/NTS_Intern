3d9166156f8d2acdb0703f7065cff9c1582de4c5
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-24 12:57:39 +0900

    Log 수정

--- src/main/java/com/nts/reservation/common/interceptor/LogInterceptor.java
+++ src/main/java/com/nts/reservation/common/interceptor/LogInterceptor.java
@@ -5,23 +5,15 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class LogInterceptor extends HandlerInterceptorAdapter {
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	private static final Logger LOGGER = LoggerFactory.getLogger(LogInterceptor.class);
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 		throws Exception {
-		logger.debug("preHandle / 호출 : {}", handler.toString());
+		LOGGER.debug("요청 URI : {}\tIP : {}", request.getRequestURI(), request.getLocalAddr());
 		return true;
 	}
-
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-		ModelAndView modelAndView) throws Exception {
-		logger.debug("postHandle / ModelAndView : {},  Handler : {} ===", modelAndView, handler.toString());
-	}
-
 }
