bcd74a0f049dcfc000d97aad52ac1a6f3e4f0ab5
박우성 <woosung.park@nts-corp.com>
2019-02-24 11:32:01 +0900

    Runtime, NoHandlerFound 예외 처리 수정

--- src/main/java/com/nts/reservation/config/WebMvcContextConfiguration.java
+++ src/main/java/com/nts/reservation/config/WebMvcContextConfiguration.java
@@ -7,11 +7,10 @@ package com.nts.reservation.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -22,14 +21,21 @@ import com.nts.reservation.interceptor.SessionInterceptor;
  */
 @Configuration
 @EnableWebMvc
-@ComponentScan({"com.nts.reservation.controller", "com.nts.reservation.error"})
+@ComponentScan("com.nts.reservation.controller")
 public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
 	/**
 	 * 31556926 seconds = 1 year
 	 * @value 31556926
 	 */
 	private static final int DEFAULT_PERIOD = 31556926;
-
+	
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		DispatcherServlet dispatcherServlet = new DispatcherServlet();
+		dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+		return dispatcherServlet;
+	}
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(DEFAULT_PERIOD);
@@ -37,16 +43,6 @@ public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
 		registry.addResourceHandler("/img_map/**").addResourceLocations("/img_map/").setCachePeriod(DEFAULT_PERIOD);
 		registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(DEFAULT_PERIOD);
 		registry.addResourceHandler("/font/**").addResourceLocations("/font/").setCachePeriod(DEFAULT_PERIOD);
-	}
-
-	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
-	}
-
-	@Override
-	public void addViewControllers(final ViewControllerRegistry registry) {
-		registry.addViewController("error").setViewName("error/invalidAccess");
 	}
 
 	@Bean
@@ -61,4 +57,6 @@ public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new SessionInterceptor());
 	}
+	
+	
 }
