7015cf6da4c3d9740b51313f769125f54d1ecd79
USER <USER@AL01288279>
2019-02-24 19:16:18 +0900

    textarea 이벤트 추가

--- src/main/webapp/js/reviewWrite/event.js
+++ src/main/webapp/js/reviewWrite/event.js
@@ -1,12 +1,21 @@
 function addReviewWriteInfoClickEvent() {
 	var reviewWriteInfo = document.querySelector(".review_contents.write > .review_write_info");
+	var reviewTextarea = document.querySelector(".review_contents.write > .review_textarea");
 	reviewWriteInfo.addEventListener("click", function(event){
 		reviewWriteInfo.style.display = "none";
-		var reviewTextarea = document.querySelector(".review_contents.write > .review_textarea");
 		reviewTextarea.focus();
 	});
 }
 
+function addReviewTextareaBlurEvent() {
+	var reviewWriteInfo = document.querySelector(".review_contents.write > .review_write_info");
+	var reviewTextarea = document.querySelector(".review_contents.write > .review_textarea");
+	reviewTextarea.addEventListener("blur", function(event){
+		if (reviewTextarea.value == "") {
+			reviewWriteInfo.style.display = "block";
+		}
+	});
+}
 
 function addRatingClickEvent() {
 	var reviewRating = document.querySelector(".review_rating.rating_point");
--- src/main/webapp/js/reviewWrite/reviewWrite.js
+++ src/main/webapp/js/reviewWrite/reviewWrite.js
@@ -7,4 +7,5 @@
 document.addEventListener("DOMContentLoaded", function() {
 	addReviewWriteInfoClickEvent();
 	addRatingClickEvent();
+	addReviewTextareaBlurEvent();
 });
