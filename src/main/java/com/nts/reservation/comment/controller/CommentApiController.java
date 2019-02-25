906f8c057325c93026f76d61a49b98d62ad38d5a
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-25 15:04:19 +0900

    리뷰 작성을 위한 post method 작성

--- src/main/java/com/nts/reservation/comment/controller/CommentApiController.java
+++ src/main/java/com/nts/reservation/comment/controller/CommentApiController.java
@@ -4,13 +4,23 @@
  */
 package com.nts.reservation.comment.controller;
 
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nts.reservation.comment.dto.Comment;
+import com.nts.reservation.comment.dto.CommentParam;
 import com.nts.reservation.comment.dto.CommentResponse;
 import com.nts.reservation.comment.service.CommentService;
 
@@ -20,6 +30,8 @@ public class CommentApiController {
 
 	@Autowired
 	private CommentService commentService;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(CommentApiController.class);
 
 	private static final int MAX_LIMIT = 50;
 
@@ -32,5 +44,21 @@ public class CommentApiController {
 		}
 
 		return commentService.getComments(displayInfoId, limit);
+	}
+
+	@PostMapping(path = "/reservations/{reservationInfoId}/comments")
+	public ResponseEntity<Comment> writeReview(@PathVariable(name = "reservationInfoId") int reservationInfoId,
+		@Valid CommentParam commentParam,
+		BindingResult bindingResult) {
+
+		if (bindingResult.hasErrors()) {
+			LOGGER.debug("{} / field : {} / code : {}", bindingResult.getFieldError().getDefaultMessage(),
+				bindingResult.getFieldError().getField(), bindingResult.getFieldError().getCode());
+			return new ResponseEntity<Comment>(HttpStatus.BAD_REQUEST);
+		}
+
+		commentService.insertComment(commentParam);
+		// TODO Object 추가
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
