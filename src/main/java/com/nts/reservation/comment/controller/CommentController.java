535602963e90432eda53a28e7ce13b4dd9554b6a
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-25 14:54:55 +0900

    comment controller 작성(리뷰 작성 form으로 이동)

--- src/main/java/com/nts/reservation/comment/controller/CommentController.java
+++ src/main/java/com/nts/reservation/comment/controller/CommentController.java
@@ -4,18 +4,36 @@
  */
 package com.nts.reservation.comment.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.nts.reservation.reserve.dto.ReservationResponse;
+import com.nts.reservation.reserve.service.ReservationService;
+
 @Controller
 public class CommentController {
-	
+
+	@Autowired
+	ReservationService reservationService;
+
+//	private static final Logger LOGGER = LoggerFactory.getLogger(CommentController.class);
+
 	@GetMapping(path = "/review/{displayInfoId}")
-	public ModelAndView goReview(@PathVariable(name = "displayInfoId") int displayInfoId, ModelAndView modelAndView) {
-		modelAndView.setViewName("review");
-		modelAndView.addObject("displayInfoId", displayInfoId);
-		return modelAndView;
+	public ModelAndView goReview(@PathVariable(name = "displayInfoId") int displayInfoId, ModelAndView mav) {
+		mav.setViewName("review");
+		mav.addObject("displayInfoId", displayInfoId);
+		return mav;
+	}
+
+	@GetMapping(path = "/reservations/{reservationInfoId}/comments")
+	public ModelAndView goWriteForm(@PathVariable(name = "reservationInfoId") int reservationInfoId, ModelAndView mav) {
+		// session, id 유무
+		ReservationResponse reservationResponse = reservationService.getReservationResponse(reservationInfoId);
+		mav.addObject(reservationResponse);
+		mav.setViewName("reviewWrite");
+		return mav;
 	}
 }
