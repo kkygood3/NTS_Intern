90e42a173942067ba299f395e850869ffa33d20d
임상현 <life4lord93@gmail.com>
2019-02-25 14:06:10 +0900

    접속 로그 개발

---
+++ src/main/java/com/nts/reservation/common/interceptor/RequestLoggingInterceptor.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2015 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.nts.reservation.common.interceptor;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class RequestLoggingInterceptor extends HandlerInterceptorAdapter {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+		throws Exception {
+		logger.debug("DATE : {}, IP : {}, URL : {}", LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+			request.getRemoteAddr(),
+			request.getRequestURL());
+		return true;
+	}
+}
