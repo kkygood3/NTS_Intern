e0ca584a44eb702bf00c1ab4a5f4b06eb631786d
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-25 14:58:06 +0900

    Comment 작성, image 파일 저장(임시)

--- src/main/java/com/nts/reservation/comment/service/serviceImpl/CommentServiceImpl.java
+++ src/main/java/com/nts/reservation/comment/service/serviceImpl/CommentServiceImpl.java
@@ -4,14 +4,19 @@
  */
 package com.nts.reservation.comment.service.serviceImpl;
 
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.nts.reservation.comment.dao.CommentDao;
 import com.nts.reservation.comment.dto.Comment;
 import com.nts.reservation.comment.dto.CommentImage;
+import com.nts.reservation.comment.dto.CommentParam;
 import com.nts.reservation.comment.dto.CommentResponse;
 import com.nts.reservation.comment.service.CommentService;
 
@@ -45,6 +50,31 @@ public class CommentServiceImpl implements CommentService {
 			.avgScore(avgScore).build();
 	}
 
+	@Override
+	@Transactional
+	public int insertComment(CommentParam commentParam) {
+		int commentId = commentDao.insertComment(commentParam);
+		int fileId = commentDao.insertFileInfo(commentParam);
+		saveFile(commentParam.getFile());
+		
+		return commentDao.insertCommentImage(commentParam.getReservationInfoId(), commentId, fileId);
+	}
+	
+	// TODO 임시 처리
+	private void saveFile(MultipartFile file) {
+		try (
+			FileOutputStream fileOutputStream = new FileOutputStream("c:/tmp/" + file.getOriginalFilename());
+			InputStream inputStream = file.getInputStream();) {
+			int readCount = 0;
+			byte[] buffer = new byte[1024];
+			while ((readCount = inputStream.read(buffer)) != -1) {
+				fileOutputStream.write(buffer, 0, readCount);
+			}
+		} catch (Exception ex) {
+			throw new RuntimeException("file Save Error");
+		}
+	}
+	
 	/**
 	 * 이메일의 아이디 부분을 split한 후 뒷글자를 블라인드 처리
 	 * @param email 
