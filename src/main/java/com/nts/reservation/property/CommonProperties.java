4e90cc76818370ea4c938e725e95d30d0e033c77
박우성 <woosung.park@nts-corp.com>
2019-02-25 10:20:38 +0900

    static, final 선언 순서 정정

--- src/main/java/com/nts/reservation/property/CommonProperties.java
+++ src/main/java/com/nts/reservation/property/CommonProperties.java
@@ -8,8 +8,8 @@ public class CommonProperties {
 	/**
 	 * 클라이언트가 페이지에 접속할 때, 한 페이지의 크기를 명시하지 않았다면 출력할 기본 item 개수  
 	 */
-	public final static String CATEGORY_DEFAULT_PAGING_LIMIT = "5";
-	public final static String DETAIL_DEFAULT_PAGING_LIMIT = "3";
+	public static final String CATEGORY_DEFAULT_PAGING_LIMIT = "5";
+	public static final String DETAIL_DEFAULT_PAGING_LIMIT = "3";
 	public static final String PRODUCT_DEFAULT_PAGING_LIMIT = "4";
 	public static final String PROMOTION_DEFAULT_PAGING_LIMIT = "20";
 	public static final String REVIEW_DEFAULT_PAGING_LIMIT = "10";
