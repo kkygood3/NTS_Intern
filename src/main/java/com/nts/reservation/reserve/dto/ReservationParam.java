73d02c3042f74ba2e276cf40e248baf3ab13a188
yongjoon.park <yongjoon.park@nts-corp.com>
2019-02-26 16:13:27 +0900

    annotation 추가

--- src/main/java/com/nts/reservation/reserve/dto/ReservationParam.java
+++ src/main/java/com/nts/reservation/reserve/dto/ReservationParam.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 import com.nts.reservation.common.utils.RegexPattern;
 
@@ -31,14 +32,17 @@ public class ReservationParam {
 	
 	@NotBlank(message="이메일 정보가 필요합니다.")	
 	@Pattern(message="이메일 형식을 잘 못 입력하였습니다.", regexp=RegexPattern.EMAIL_REGEX)
+	@Size(min = 9, max = 254)
 	private String reservationEmail;
 	
 	@Pattern(message="이름 정보를 다시 확인해주세요.", regexp=RegexPattern.NAME_REGEX)
 	@NotBlank(message="이름 정보가 필요합니다.")
+	@Size(min = 2, max = 254)
 	private String reservationName;
 	
 	@Pattern(message="전화번호 형식을 잘 못 입력하였습니다.", regexp=RegexPattern.TEL_REGEX)
 	@NotBlank(message="전화번호 정보가 필요합니다.")
+	@Size(min = 10, max = 11)
 	private String reservationTel;
 	
 	@NotBlank(message="예약 날짜 정보가 필요합니다.")
