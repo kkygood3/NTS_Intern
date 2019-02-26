db065eab2c3a374b1e8dc4a27dff430cad75b074
임상현 <life4lord93@gmail.com>
2019-02-26 14:34:43 +0900

    불필요 import 삭제

--- src/main/java/com/nts/reservation/comment/model/WritedComment.java
+++ src/main/java/com/nts/reservation/comment/model/WritedComment.java
@@ -7,10 +7,7 @@ package com.nts.reservation.comment.model;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-
-import com.nts.reservation.common.regexp.Regexp;
 
 public class WritedComment {
 
