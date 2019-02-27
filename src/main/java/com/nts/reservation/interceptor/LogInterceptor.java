c43a9fbbba5fb745b0eac6871f67f257bd159fab
siyoon <c.yun@nts-corp.com>
2019-02-27 11:00:01 +0900

    불필요한 코드 제거

--- src/main/java/com/nts/reservation/interceptor/LogInterceptor.java
+++ src/main/java/com/nts/reservation/interceptor/LogInterceptor.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class LogInterceptor extends HandlerInterceptorAdapter {
